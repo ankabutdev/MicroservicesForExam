95a27b08be39ba31eec3198e4506739ee79b9ff3
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 18:41:50 +0500

    Added Configurations

---
+++ src/GameClub/GameClub.Infrastructure/Persistence/EntityTypeConfiguration/ScheduleOfChangesConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using GameClub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GameClub.Infrastructure.Persistence.EntityTypeConfiguration;
+
+public class ScheduleOfChangesConfiguration : IEntityTypeConfiguration<ScheduleOfChanges>
+{
+    public void Configure(EntityTypeBuilder<ScheduleOfChanges> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .UseIdentityColumn();
+
+        builder.Property(x => x.Status)
+            .IsRequired();
+
+        builder.Property(x => x.Description)
+            .HasMaxLength(256);
+
+        builder.HasOne(x => x.Admin)
+            .WithMany(x => x.ScheduleOfChanges);
+
+    }
+}
