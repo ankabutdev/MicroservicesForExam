ab63df3001d51360ddd8fd94f1b25268199ba7d1
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 21:52:55 +0500

    added query

---
+++ src/GameClub/GameClub.Application/UseCases/PlayerCases/Queries/PlayerGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using GameClub.Domain.Entities;
+using MediatR;
+
+namespace GameClub.Application.UseCases.PlayerCases.Queries;
+
+public class PlayerGetByIdQuery : IRequest<Player>
+{
+    public long Id { get; set; }
+}
