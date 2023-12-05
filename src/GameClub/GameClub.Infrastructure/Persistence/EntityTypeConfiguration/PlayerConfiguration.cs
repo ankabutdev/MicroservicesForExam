ccdc8d46f3b280582fb8910a571af3cf13403613
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 18:41:45 +0500

    Added Configurations

---
+++ src/GameClub/GameClub.Infrastructure/Persistence/EntityTypeConfiguration/PlayerConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using GameClub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GameClub.Infrastructure.Persistence.EntityTypeConfiguration;
+
+public class PlayerConfiguration : IEntityTypeConfiguration<Player>
+{
+    public void Configure(EntityTypeBuilder<Player> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .UseIdentityColumn();
+
+        builder.HasIndex(x => x.NickName)
+            .IsUnique();
+
+        builder.HasMany(x => x.ScheduleOfChanges)
+            .WithOne(x => x.Player);
+    }
+}
