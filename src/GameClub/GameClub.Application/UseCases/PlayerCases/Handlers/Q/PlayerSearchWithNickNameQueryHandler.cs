d229e5c764f4fff04a2b2641beb9ec3838f37272
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 21:52:35 +0500

    implement handler

---
+++ src/GameClub/GameClub.Application/UseCases/PlayerCases/Handlers/Q/PlayerSearchWithNickNameQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using GameClub.Application.Abstractions;
+using GameClub.Application.UseCases.PlayerCases.Queries;
+using GameClub.Domain.Entities;
+using MediatR;
+
+namespace GameClub.Application.UseCases.PlayerCases.Handlers.Q;
+
+public class PlayerSearchWithNickNameQueryHandler : IRequestHandler<PlayerSearchWithNickNameQuery, IQueryable<Player>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public PlayerSearchWithNickNameQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IQueryable<Player>> Handle(PlayerSearchWithNickNameQuery request,
+        CancellationToken cancellationToken)
+    {
+        var players = _context.Players
+            .Where(x => x.NickName.ToLower()
+            .Contains(request.NickName.ToLower()));
+
+        return players;
+    }
+}
