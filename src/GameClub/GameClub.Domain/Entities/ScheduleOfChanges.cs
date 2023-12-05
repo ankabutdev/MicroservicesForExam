e1b64b8b3d9fb3b645f52b4d13d470648e1c7fbe
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 18:41:05 +0500

    improve

--- src/GameClub/GameClub.Domain/Entities/ScheduleOfChanges.cs
+++ src/GameClub/GameClub.Domain/Entities/ScheduleOfChanges.cs
@@ -1,23 +1,21 @@
 ﻿using GameClub.Domain.Enums;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GameClub.Domain.Entities;
 
 public class ScheduleOfChanges : BaseEntity
 {
-    public IEnumerable<Computer> Computers { get; set; }
-
-    public IEnumerable<Player> Players { get; set; }
-
     public string Description { get; set; }
 
     public double TotalPrice { get; set; }
 
     public ChangesStatus Status { get; set; }
 
-    public long HistoryId { get; set; }
+    public long AdminId { get; set; }
 
-    [ForeignKey(nameof(HistoryId))]
-    public History History { get; set; }
+    public Admin Admin { get; set; }
+
+    public long PlayerId { get; set; }
+
+    public Player Player { get; set; }
 
 }
