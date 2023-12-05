f3aebc7c0ae3d971c9339cf0fba47bea14ebdba8
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 21:52:59 +0500

    added query

---
+++ src/GameClub/GameClub.Application/UseCases/PlayerCases/Queries/PlayerSearchWithNickNameQuery.cs
@@ -0,0 +1,9 @@
+﻿using GameClub.Domain.Entities;
+using MediatR;
+
+namespace GameClub.Application.UseCases.PlayerCases.Queries;
+
+public class PlayerSearchWithNickNameQuery : IRequest<IQueryable<Player>>
+{
+    public string NickName { get; set; }
+}
