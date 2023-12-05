e956b4f264bfd99ce571330023edc0f7660ee142
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 21:51:29 +0500

    improve

---
+++ src/GameClub/GameClub.Application/UseCases/PlayerCases/Commands/PlayerCreateCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace GameClub.Application.UseCases.PlayerCases.Commands;
+
+public class PlayerCreateCommand : IRequest<bool>
+{
+    public string NickName { get; set; }
+
+    public long HoursCount { get; set; }
+
+    public long ComputerId { get; set; }
+}
