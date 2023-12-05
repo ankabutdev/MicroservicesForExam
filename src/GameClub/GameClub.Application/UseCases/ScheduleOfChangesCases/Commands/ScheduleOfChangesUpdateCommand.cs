038136d1ac2dc25d63c722a687b615d3bfee4da4
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 21:54:14 +0500

    improve

---
+++ src/GameClub/GameClub.Application/UseCases/ScheduleOfChangesCases/Commands/ScheduleOfChangesUpdateCommand.cs
@@ -0,0 +1,17 @@
+﻿using GameClub.Domain.Enums;
+using MediatR;
+
+namespace GameClub.Application.UseCases.ScheduleOfChangesCases.Commands;
+
+public class ScheduleOfChangesUpdateCommand : IRequest<bool>
+{
+    public string Description { get; set; }
+
+    public double TotalPrice { get; set; }
+
+    public ChangesStatus Status { get; set; }
+
+    public long AdminId { get; set; }
+
+    public long PlayerId { get; set; }
+}
