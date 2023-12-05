1fb1c226f81cf856fe3a1829b03e84a40e93108b
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 17:42:23 +0500

    Added and improved mapping profile

--- src/GameClub/GameClub.Application/Mappers/MappingProfile.cs
+++ src/GameClub/GameClub.Application/Mappers/MappingProfile.cs
@@ -1,6 +1,10 @@
 ﻿using AutoMapper;
 using GameClub.Application.UseCases.AdminCases.Commands;
+using GameClub.Application.UseCases.ComputerCases.Commands;
+using GameClub.Application.UseCases.HistoryCases.Commands;
 using GameClub.Domain.DTOs.Admins;
+using GameClub.Domain.DTOs.Computers;
+using GameClub.Domain.DTOs.Histories;
 using GameClub.Domain.Entities;
 
 namespace GameClub.Application.Mappers;
@@ -16,6 +20,21 @@ public class MappingProfile : Profile
         CreateMap<CreateAdminCommand, AdminCreateDto>().ReverseMap();
         CreateMap<UpdateAdminCommand, AdminUpdateDto>().ReverseMap();
 
+        // Computers
+        CreateMap<Computer, ComputerCreateCommand>().ReverseMap();
+        CreateMap<Computer, ComputerUpdateCommand>().ReverseMap();
+
+        CreateMap<ComputerCreateCommand, ComputerCreateDto>().ReverseMap();
+        CreateMap<ComputerUpdateCommand, ComputerUpdateDto>().ReverseMap();
+
+        // Histories
+        CreateMap<History, HistoryCreateCommand>().ReverseMap();
+        CreateMap<History, HistoryUpdateCommand>().ReverseMap();
+
+        CreateMap<HistoryCreateCommand, HistoryCreateDto>().ReverseMap();
+        CreateMap<HistoryUpdateCommand, HistoryUpdateDto>().ReverseMap();
+
         // ...
+
     }
 }
