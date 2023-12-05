2d5447de55166c1ace5d45c74d97a99d6b28dde1
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 21:52:26 +0500

    implement handler

---
+++ src/GameClub/GameClub.Application/UseCases/PlayerCases/Handlers/Q/PlayerGetAllQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using GameClub.Application.Abstractions;
+using GameClub.Application.UseCases.PlayerCases.Queries;
+using GameClub.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameClub.Application.UseCases.PlayerCases.Handlers.Q;
+
+public class PlayerGetAllQueryHandler : IRequestHandler<PlayerGetAllQuery, IEnumerable<Player>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public PlayerGetAllQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Player>> Handle(PlayerGetAllQuery request,
+        CancellationToken cancellationToken)
+    {
+        return await _context.Players.ToListAsync(cancellationToken);
+    }
+}
