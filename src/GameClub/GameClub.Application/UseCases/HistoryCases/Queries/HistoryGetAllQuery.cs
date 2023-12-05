b2d76a500351231a2216be4cc421049d08806716
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 18:40:32 +0500

    improve

--- src/GameClub/GameClub.Application/UseCases/HistoryCases/Queries/HistoryGetAllQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿using GameClub.Domain.Entities;
-using MediatR;
-
-namespace GameClub.Application.UseCases.HistoryCases.Queries;
-
-public class HistoryGetAllQuery : IRequest<IEnumerable<History>>
-{
-}
