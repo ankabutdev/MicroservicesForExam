b109e682bdf3c35ac693cd70488e018860774c04
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 21:50:02 +0500

    added classes

--- src/GameClub/GameClub.Application/Mappers/MappingProfile.cs
+++ src/GameClub/GameClub.Application/Mappers/MappingProfile.cs
@@ -1,8 +1,12 @@
 ﻿using AutoMapper;
 using GameClub.Application.UseCases.AdminCases.Commands;
 using GameClub.Application.UseCases.ComputerCases.Commands;
+using GameClub.Application.UseCases.PlayerCases.Commands;
+using GameClub.Application.UseCases.ScheduleOfChangesCases.Commands;
 using GameClub.Domain.DTOs.Admins;
 using GameClub.Domain.DTOs.Computers;
+using GameClub.Domain.DTOs.Players;
+using GameClub.Domain.DTOs.ScheduleOfChanges;
 using GameClub.Domain.Entities;
 
 namespace GameClub.Application.Mappers;
@@ -25,7 +29,19 @@ public class MappingProfile : Profile
         CreateMap<ComputerCreateCommand, ComputerCreateDto>().ReverseMap();
         CreateMap<ComputerUpdateCommand, ComputerUpdateDto>().ReverseMap();
 
-        // ...
+        // Players
+        CreateMap<Player, PlayerCreateCommand>().ReverseMap();
+        CreateMap<Player, PlayerUpdateCommand>().ReverseMap();
+
+        CreateMap<PlayerCreateCommand, PlayerCreateDto>().ReverseMap();
+        CreateMap<PlayerUpdateCommand, PlayerUpdateDto>().ReverseMap();
+
+        // ScheduleOfChanges
+        CreateMap<ScheduleOfChanges, ScheduleOfChangesCreateCommand>().ReverseMap();
+        CreateMap<ScheduleOfChanges, ScheduleOfChangesUpdateCommand>().ReverseMap();
+
+        CreateMap<ScheduleOfChangesCreateCommand, ScheduleOfChangesCreateDto>().ReverseMap();
+        CreateMap<ScheduleOfChangesUpdateCommand, ScheduleOfChangesUpdateDto>().ReverseMap();
 
     }
 }
