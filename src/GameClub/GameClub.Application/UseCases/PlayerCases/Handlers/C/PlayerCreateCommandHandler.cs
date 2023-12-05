6d3afc92960a5ccfeb3206e90f1d502136edd72e
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 21:51:57 +0500

    Add Handlers

---
+++ src/GameClub/GameClub.Application/UseCases/PlayerCases/Handlers/C/PlayerCreateCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using GameClub.Application.Abstractions;
+using GameClub.Application.UseCases.PlayerCases.Commands;
+using GameClub.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameClub.Application.UseCases.PlayerCases.Handlers.C;
+
+public class PlayerCreateCommandHandler : IRequestHandler<PlayerCreateCommand, bool>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public PlayerCreateCommandHandler(IApplicationDbContext context,
+        IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<bool> Handle(PlayerCreateCommand request,
+        CancellationToken cancellationToken)
+    {
+        var player = await _context.Players
+            .FirstOrDefaultAsync(x => x.NickName == request.NickName);
+
+        if (player is not null)
+            return false;
+
+        player = _mapper.Map<Player>(request);
+
+        await _context.Players.AddAsync(player, cancellationToken);
+        var result = await _context
+            .SaveChangesAsync(cancellationToken);
+
+        return result > 0;
+    }
+}
