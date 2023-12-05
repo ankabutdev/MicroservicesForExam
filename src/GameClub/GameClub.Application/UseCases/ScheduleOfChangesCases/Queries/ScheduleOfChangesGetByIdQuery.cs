a4c5fcf4c789f27d0ab62801733c098470537797
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 21:54:55 +0500

    implement query

---
+++ src/GameClub/GameClub.Application/UseCases/ScheduleOfChangesCases/Queries/ScheduleOfChangesGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using GameClub.Domain.Entities;
+using MediatR;
+
+namespace GameClub.Application.UseCases.ScheduleOfChangesCases.Queries;
+
+public class ScheduleOfChangesGetByIdQuery : IRequest<ScheduleOfChanges>
+{
+    public long Id { get; set; }
+}
