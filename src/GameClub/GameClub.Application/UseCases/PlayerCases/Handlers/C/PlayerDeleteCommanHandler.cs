afdfb0f8f14fbe0c56efd18c55d6e65d38e0556f
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 21:52:00 +0500

    Add Handlers

---
+++ src/GameClub/GameClub.Application/UseCases/PlayerCases/Handlers/C/PlayerDeleteCommanHandler.cs
@@ -0,0 +1,35 @@
+﻿using GameClub.Application.Abstractions;
+using GameClub.Application.UseCases.PlayerCases.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameClub.Application.UseCases.PlayerCases.Handlers.C;
+
+public class PlayerDeleteCommanHandler : IRequestHandler<PlayerDeleteCommand, bool>
+{
+    private readonly IApplicationDbContext _context;
+
+    public PlayerDeleteCommanHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> Handle(PlayerDeleteCommand request, CancellationToken cancellationToken)
+    {
+        if (request.Id <= 0)
+            return false;
+
+        var player = await _context.Players
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+        if (player == null)
+            return false;
+
+        _context.Players.Remove(player);
+
+        var result = await _context
+            .SaveChangesAsync(cancellationToken);
+
+        return result > 0;
+    }
+}
