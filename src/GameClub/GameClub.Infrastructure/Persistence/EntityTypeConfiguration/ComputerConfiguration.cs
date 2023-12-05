1d03e7cc2a635311e29547d20e9cc8ff2a7b43ab
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 18:41:29 +0500

    improve

---
+++ src/GameClub/GameClub.Infrastructure/Persistence/EntityTypeConfiguration/ComputerConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using GameClub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GameClub.Infrastructure.Persistence.EntityTypeConfiguration;
+
+public class ComputerConfiguration : IEntityTypeConfiguration<Computer>
+{
+    public void Configure(EntityTypeBuilder<Computer> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .UseIdentityColumn();
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(256)
+            .IsRequired();
+
+        builder.HasMany(x => x.Players)
+            .WithOne(x => x.Computer);
+
+        builder.HasIndex(x => x.Version)
+            .IsUnique();
+
+        builder.Property(x => x.Version)
+            .IsRequired();
+
+        builder.Property(x => x.PriceOfHour)
+            .IsRequired();
+
+        builder.HasMany(x => x.Players)
+            .WithOne(x => x.Computer);
+    }
+}
