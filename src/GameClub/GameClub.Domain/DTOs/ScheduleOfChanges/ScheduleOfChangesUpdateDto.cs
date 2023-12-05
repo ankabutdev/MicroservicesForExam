e6bc06a36134b8f47b6f309c04a49113fcb535fa
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 21:55:48 +0500

    added dtos

---
+++ src/GameClub/GameClub.Domain/DTOs/ScheduleOfChanges/ScheduleOfChangesUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using GameClub.Domain.Enums;
+
+namespace GameClub.Domain.DTOs.ScheduleOfChanges;
+
+public class ScheduleOfChangesUpdateDto
+{
+    public string Description { get; set; }
+
+    public double TotalPrice { get; set; }
+
+    public ChangesStatus Status { get; set; }
+
+    public long AdminId { get; set; }
+
+    public long PlayerId { get; set; }
+}
