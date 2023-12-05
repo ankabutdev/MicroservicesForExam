143c7599b4ad77840aa45a3ea3177bccb845b001
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 22:13:51 +0500

    Completed GameClub Service

--- src/GameClub/GameClub.API/Controllers/PlayersController.cs
+++ src/GameClub/GameClub.API/Controllers/PlayersController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using GameClub.Application.UseCases.PlayerCases.Commands;
+using GameClub.Application.UseCases.PlayerCases.Queries;
 using GameClub.Domain.DTOs.Players;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -20,12 +21,62 @@ public class PlayersController : ControllerBase
         _mediator = mediator;
     }
 
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var players = await _mediator
+            .Send(new PlayerGetAllQuery());
+
+        return Ok(players);
+    }
+
+    [HttpGet("nickname")]
+    public async Task<IActionResult> GetByNickNameAsync(string nickname)
+    {
+        var player = await _mediator
+            .Send(new PlayerSearchWithNickNameQuery() { NickName = nickname });
+
+        return Ok(player);
+    }
+
+    [HttpGet("{Id}")]
+    public async Task<IActionResult> GetByIdAsync(long Id)
+    {
+        var player = await _mediator
+            .Send(new PlayerGetByIdQuery() { Id = Id });
+
+        return Ok(player);
+    }
+
     [HttpPost]
     public async Task<IActionResult> CreateAsync(PlayerCreateDto dto)
     {
         var player = _mapper.Map<PlayerCreateCommand>(dto);
 
         var result = await _mediator.Send(player);
+
+        return Ok(result);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateAsync(PlayerUpdateDto dto)
+    {
+        var player = _mapper.Map<PlayerUpdateCommand>(dto);
+
+        var result = await _mediator.Send(player);
+
+        return Ok(result);
+    }
+
+    [HttpDelete("{Id}")]
+    public async Task<IActionResult> DeleteAsync(long Id)
+    {
+        var result = await _mediator
+            .Send(
+            new PlayerDeleteCommand()
+            {
+                Id = Id
+            });
 
         return Ok(result);
     }
---
+++ src/GameClub/GameClub.API/Controllers/ScheduleOfChangesController.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using GameClub.Application.UseCases.ScheduleOfChangesCases.Commands;
+using GameClub.Application.UseCases.ScheduleOfChangesCases.Queries;
+using GameClub.Domain.DTOs.ScheduleOfChanges;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameClub.API.Controllers;
+
+[Route("api/sochs")]
+[ApiController]
+public class ScheduleOfChangesController : ControllerBase
+{
+    private readonly IMapper _mapper;
+    private readonly IMediator _mediator;
+
+    public ScheduleOfChangesController(IMediator mediator,
+        IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var sochs = await _mediator
+            .Send(new ScheduleOfChangesGetAllQuery());
+
+        return Ok(sochs);
+    }
+
+    [HttpGet("{Id}")]
+    public async Task<IActionResult> GetByIdAsync(long Id)
+    {
+        var soch = await _mediator
+            .Send(new ScheduleOfChangesGetByIdQuery() { Id = Id });
+
+        return Ok(soch);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsnyc(ScheduleOfChangesCreateDto dto)
+    {
+        var soch = _mapper.Map<ScheduleOfChangesCreateCommand>(dto);
+
+        var result = await _mediator.Send(soch);
+
+        return Ok(result);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateAsync(ScheduleOfChangesUpdateDto dto)
+    {
+        var soch = _mapper.Map<ScheduleOfChangesCreateCommand>(dto);
+
+        var result = await _mediator.Send(soch);
+
+        return Ok(result);
+    }
+
+    [HttpDelete("{Id}")]
+    public async Task<IActionResult> DeleteAsync(long Id)
+    {
+        var result = await _mediator
+            .Send(
+            new ScheduleOfChangesDeleteCommand()
+            {
+                Id = Id
+            });
+
+        return Ok(result);
+    }
+}
