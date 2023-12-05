49f12c1f5a500bea8244c87cf5ff61373522f487
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 21:55:31 +0500

    Added dtos

---
+++ src/GameClub/GameClub.Domain/DTOs/Players/PlayerCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GameClub.Domain.DTOs.Players;
+
+public class PlayerCreateDto
+{
+    public string NickName { get; set; }
+
+    public long HoursCount { get; set; }
+
+    public long ComputerId { get; set; }
+}
---
+++ src/GameClub/GameClub.Domain/DTOs/Players/PlayerUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GameClub.Domain.DTOs.Players;
+
+public class PlayerUpdateDto
+{
+    public string NickName { get; set; }
+
+    public long HoursCount { get; set; }
+}
