bf52c4dcbeafa57a3f2999eb1ac20b4fe1ec0c12
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 21:54:49 +0500

    implement query

---
+++ src/GameClub/GameClub.Application/UseCases/ScheduleOfChangesCases/Queries/ScheduleOfChangesGetAllQuery.cs
@@ -0,0 +1,8 @@
+﻿using GameClub.Domain.Entities;
+using MediatR;
+
+namespace GameClub.Application.UseCases.ScheduleOfChangesCases.Queries;
+
+public class ScheduleOfChangesGetAllQuery : IRequest<IEnumerable<ScheduleOfChanges>>
+{
+}
