6ea7b38ee68fabec0dcc770c8628f7c04ee3dda0
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 21:54:26 +0500

    implement handler

---
+++ src/GameClub/GameClub.Application/UseCases/ScheduleOfChangesCases/Handlers/ScheduleOfChangesDeleteCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using GameClub.Application.Abstractions;
+using GameClub.Application.UseCases.ScheduleOfChangesCases.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameClub.Application.UseCases.ScheduleOfChangesCases.Handlers;
+
+public class ScheduleOfChangesDeleteCommandHandler :
+    IRequestHandler<ScheduleOfChangesDeleteCommand, bool>
+{
+    private readonly IApplicationDbContext _context;
+
+    public ScheduleOfChangesDeleteCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> Handle(ScheduleOfChangesDeleteCommand request,
+        CancellationToken cancellationToken)
+    {
+        if (request.Id <= 0)
+            return false;
+
+        var soch = await _context.ScheduleOfChanges
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+        if (soch == null)
+            return false;
+
+        _context.ScheduleOfChanges.Remove(soch);
+
+        var result = await _context
+            .SaveChangesAsync(cancellationToken);
+
+        return result > 0;
+    }
+}
