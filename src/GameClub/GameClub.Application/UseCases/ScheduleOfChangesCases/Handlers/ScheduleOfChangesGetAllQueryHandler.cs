6bd7ac15e58f77ecad8aa71622c75d7b6984af32
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 21:54:31 +0500

    implement handler

---
+++ src/GameClub/GameClub.Application/UseCases/ScheduleOfChangesCases/Handlers/ScheduleOfChangesGetAllQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using GameClub.Application.Abstractions;
+using GameClub.Application.UseCases.ScheduleOfChangesCases.Queries;
+using GameClub.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameClub.Application.UseCases.ScheduleOfChangesCases.Handlers;
+
+public class ScheduleOfChangesGetAllQueryHandler :
+    IRequestHandler<ScheduleOfChangesGetAllQuery, IEnumerable<ScheduleOfChanges>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public ScheduleOfChangesGetAllQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<ScheduleOfChanges>> Handle(ScheduleOfChangesGetAllQuery request,
+        CancellationToken cancellationToken)
+    {
+        return await _context.ScheduleOfChanges.ToListAsync(cancellationToken);
+    }
+}
