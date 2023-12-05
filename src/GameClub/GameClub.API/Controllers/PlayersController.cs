7b47bcec85e994154434ee27fda67003e270ead1
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 21:49:49 +0500

    improve

---
+++ src/GameClub/GameClub.API/Controllers/PlayersController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using GameClub.Application.UseCases.PlayerCases.Commands;
+using GameClub.Domain.DTOs.Players;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameClub.API.Controllers;
+
+[Route("api/players")]
+[ApiController]
+public class PlayersController : ControllerBase
+{
+    private readonly IMapper _mapper;
+    private readonly IMediator _mediator;
+
+    public PlayersController(IMapper mapper,
+        IMediator mediator)
+    {
+        _mapper = mapper;
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(PlayerCreateDto dto)
+    {
+        var player = _mapper.Map<PlayerCreateCommand>(dto);
+
+        var result = await _mediator.Send(player);
+
+        return Ok(result);
+    }
+}
