90c20302ee8b627783ca4b7d8249ed03f7bc6f1c
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-05 21:52:52 +0500

    added query

---
+++ src/GameClub/GameClub.Application/UseCases/PlayerCases/Queries/PlayerGetAllQuery.cs
@@ -0,0 +1,8 @@
+﻿using GameClub.Domain.Entities;
+using MediatR;
+
+namespace GameClub.Application.UseCases.PlayerCases.Queries;
+
+public class PlayerGetAllQuery : IRequest<IEnumerable<Player>>
+{
+}
