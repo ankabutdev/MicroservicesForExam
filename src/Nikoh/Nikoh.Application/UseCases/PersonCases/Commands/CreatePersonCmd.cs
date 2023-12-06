24dccda076dd657b83408c136de6a219821d92ac
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:38:48 +0500

    Improve

---
+++ src/Nikoh/Nikoh.Application/UseCases/PersonCases/Commands/CreatePersonCmd.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using NIkoh.Domain.Enums;
+
+namespace Nikoh.Application.UseCases.PersonCases.Commands;
+
+public class CreatePersonCmd : IRequest<bool>
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string MiddleName { get; set; }
+
+    public string PassportSeriaNumber { get; set; }
+
+    public long Age { get; set; }
+
+    public string Address { get; set; }
+
+    public string Healthy { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+
+    public Gender Gender { get; set; }
+
+}
