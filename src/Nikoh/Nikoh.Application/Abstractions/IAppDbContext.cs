a5acd81dbf2e1c9e6e7d2b0d5bb53a5d8168029b
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:37:42 +0500

    added interface in Abstractions

---
+++ src/Nikoh/Nikoh.Application/Abstractions/IAppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NIkoh.Domain.Entities.Marriages;
+using NIkoh.Domain.Entities.Persons;
+using NIkoh.Domain.Entities.Requirements;
+
+namespace Nikoh.Application.Abstractions;
+
+public interface IAppDbContext
+{
+    public DbSet<Person> People { get; set; }
+
+    public DbSet<Marriage> Marriages { get; set; }
+
+    public DbSet<Requirement> Requirements { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
