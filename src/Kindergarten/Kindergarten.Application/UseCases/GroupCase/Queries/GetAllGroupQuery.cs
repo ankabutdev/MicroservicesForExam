d50637d56391723eff0d29880314056bb41f8b22
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:39:12 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/GroupCase/Queries/GetAllGroupQuery.cs
@@ -0,0 +1,8 @@
+﻿using Kindergarten.Domain.Entities.Groups;
+using MediatR;
+
+namespace Kindergarten.Application.UseCases.GroupCase.Queries;
+
+public class GetAllGroupQuery : IRequest<IEnumerable<Group>>
+{
+}
