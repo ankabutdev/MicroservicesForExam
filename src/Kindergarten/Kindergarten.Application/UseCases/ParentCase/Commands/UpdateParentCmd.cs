3daec8a1854df8c6f23c6be657a220f83097532b
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:39:41 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/ParentCase/Commands/UpdateParentCmd.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace Kindergarten.Application.UseCases.ParentCase.Commands;
+
+public class UpdateParentCmd : IRequest<bool>
+{
+    public long Id { get; set; }
+
+    public string MotherFullName { get; set; }
+
+    public string FatherFullName { get; set; }
+
+    public string PassportSeriaNumber { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string Email { get; set; }
+
+    public string Address { get; set; }
+}
