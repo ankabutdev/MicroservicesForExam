ee472a9b2a5e4faa4db78356252be2d0b193cac7
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:38:34 +0500

    Improve

---
+++ src/Nikoh/Nikoh.Application/UseCases/MarriageCases/Handlers/Q/GetAllMarriageQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Nikoh.Application.Abstractions;
+using Nikoh.Application.UseCases.MarriageCases.Queris;
+using NIkoh.Domain.Entities.Marriages;
+
+namespace Nikoh.Application.UseCases.MarriageCases.Handlers.Q;
+
+public class GetAllMarriageQueryHandler :
+    IRequestHandler<GetAllMarriageQuery, IEnumerable<Marriage>>
+{
+    private readonly IAppDbContext _context;
+
+    public GetAllMarriageQueryHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Marriage>> Handle(GetAllMarriageQuery request,
+        CancellationToken cancellationToken)
+    {
+        return await _context.Marriages.ToListAsync(cancellationToken);
+    }
+}
