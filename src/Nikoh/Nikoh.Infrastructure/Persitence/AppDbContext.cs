c39009fc55e63fa3e7f9c6c82e60d470403c3da4
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:41:07 +0500

    Added AppDbContext

---
+++ src/Nikoh/Nikoh.Infrastructure/Persitence/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nikoh.Application.Abstractions;
+using NIkoh.Domain.Entities.Marriages;
+using NIkoh.Domain.Entities.Persons;
+using NIkoh.Domain.Entities.Requirements;
+using System.Reflection;
+
+namespace Nikoh.Infrastructure.Persitence;
+
+public class AppDbContext : DbContext, IAppDbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+    public virtual DbSet<Person> People { get; set; }
+
+    public virtual DbSet<Requirement> Requirements { get; set; }
+
+    public virtual DbSet<Marriage> Marriages { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
