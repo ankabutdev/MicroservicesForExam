e599750a216273da7415be94a16a97750a2be0da
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:41:07 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/DepandecyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Kindergarten.Application.Mappers;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Kindergarten.Application;
+
+public static class DepandecyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddAutoMapper(typeof(MappingConfiguration));
+        return services;
+    }
+}
