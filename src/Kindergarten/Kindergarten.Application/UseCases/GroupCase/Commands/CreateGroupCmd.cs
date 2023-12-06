0a2d4634ebbbdf7a4c2633d6c5756fc3e1603df2
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:38:22 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/GroupCase/Commands/CreateGroupCmd.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Kindergarten.Application.UseCases.GroupCase.Commands;
+
+public class CreateGroupCmd : IRequest<bool>
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public long AgeGroup { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+}
