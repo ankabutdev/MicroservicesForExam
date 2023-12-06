30aee6c6dc3c82dbf7e5cfd914e9be7f3d802520
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:42:01 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Domain/Entities/Parents/Parent.cs
@@ -0,0 +1,20 @@
+﻿using Kindergarten.Domain.Entities.Students;
+
+namespace Kindergarten.Domain.Entities.Parents;
+
+public class Parent : BaseEntity
+{
+    public string MotherFullName { get; set; }
+
+    public string FatherFullName { get; set; }
+
+    public string PassportSeriaNumber { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string Email { get; set; }
+
+    public string Address { get; set; }
+
+    public IEnumerable<Student> Students { get; set; }
+}
