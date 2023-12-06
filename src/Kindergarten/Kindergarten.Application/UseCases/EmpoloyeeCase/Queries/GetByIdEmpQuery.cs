e4875685e056c6f9fdf888ee4c7c0fddeb9dca5d
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:38:10 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/EmpoloyeeCase/Queries/GetByIdEmpQuery.cs
@@ -0,0 +1,9 @@
+﻿using Kindergarten.Domain.Entities.Employees;
+using MediatR;
+
+namespace Kindergarten.Application.UseCases.EmpoloyeeCase.Queries;
+
+public class GetByIdEmpQuery : IRequest<Employee>
+{
+    public long Id { get; set; }
+}
