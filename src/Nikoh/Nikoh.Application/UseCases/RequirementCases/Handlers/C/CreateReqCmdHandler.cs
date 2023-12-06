069a4d38168e5a80105c6caae74db1a7db1f56a3
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:39:30 +0500

    Improve

---
+++ src/Nikoh/Nikoh.Application/UseCases/RequirementCases/Handlers/C/CreateReqCmdHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MediatR;
+using Nikoh.Application.Abstractions;
+using Nikoh.Application.UseCases.RequirementCases.Commands;
+using NIkoh.Domain.Entities.Requirements;
+
+namespace Nikoh.Application.UseCases.RequirementCases.Handlers.C;
+
+public class CreateReqCmdHandler : IRequestHandler<CreateReqCmd, bool>
+{
+    private readonly IAppDbContext _context;
+    private readonly IMapper _mapper;
+
+    public CreateReqCmdHandler(IAppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<bool> Handle(CreateReqCmd request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var entity = _mapper.Map<Requirement>(request);
+
+            await _context.Requirements.AddAsync(entity);
+            var result = await _context.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
