634c7fe7f2c07727c46004d946f978f8b7540913
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:40:16 +0500

    Improve

---
+++ src/Nikoh/NIkoh.Domain/Dtos/Persons/CreatePersonDto.cs
@@ -0,0 +1,25 @@
+﻿using NIkoh.Domain.Enums;
+
+namespace NIkoh.Domain.Dtos.Persons;
+
+public class CreatePersonDto
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string MiddleName { get; set; }
+
+    public string PassportSeriaNumber { get; set; }
+
+    public long Age { get; set; }
+
+    public string Address { get; set; }
+
+    public string Healthy { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+
+    public Gender Gender { get; set; }
+
+}
