12147e5ef74c175281f43c712341351dc3f46840
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:39:16 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/GroupCase/Queries/GetByIdGroupQuery.cs
@@ -0,0 +1,9 @@
+﻿using Kindergarten.Domain.Entities.Groups;
+using MediatR;
+
+namespace Kindergarten.Application.UseCases.GroupCase.Queries;
+
+public class GetByIdGroupQuery : IRequest<Group>
+{
+    public long Id { get; set; }
+}
