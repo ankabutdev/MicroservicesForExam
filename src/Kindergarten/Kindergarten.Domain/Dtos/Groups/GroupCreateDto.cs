1b36303b7a89d92dc2b01f5302f76cff86b35c92
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:41:28 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Domain/Dtos/Groups/GroupCreateDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Kindergarten.Domain.Dtos.Groups;
+
+public class GroupCreateDto
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public long AgeGroup { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+}
