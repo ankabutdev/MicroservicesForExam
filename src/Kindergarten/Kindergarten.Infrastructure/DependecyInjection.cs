ac5d97a9de3862cd42d109a7237cba193604f3eb
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:42:49 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Infrastructure/DependecyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Kindergarten.Application.Abstractions;
+using Kindergarten.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kindergarten.Infrastructure;
+
+public static class DependecyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<IAppDbContext, AppDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        return services;
+    }
+}
