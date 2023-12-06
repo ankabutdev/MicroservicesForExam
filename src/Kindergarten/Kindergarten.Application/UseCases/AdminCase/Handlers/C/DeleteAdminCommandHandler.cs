a2b042429e6fb67153610fd215b362a35e31f396
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:36:59 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/AdminCase/Handlers/C/DeleteAdminCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Kindergarten.Application.Abstractions;
+using Kindergarten.Application.UseCases.AdminCases.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kindergarten.Application.UseCases.AdminCase.Handlers.C;
+
+public class AdminDeleteCommandHandler : IRequestHandler<DeleteAdminCommand, bool>
+{
+    private readonly IAppDbContext _context;
+
+    public AdminDeleteCommandHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> Handle(DeleteAdminCommand request, CancellationToken cancellationToken)
+    {
+        if (request.Id <= 0)
+            return false;
+
+        var admin = await _context.Admins
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+        if (admin == null)
+            return false;
+
+        _context.Admins.Remove(admin);
+
+        var result = await _context
+            .SaveChangesAsync(cancellationToken);
+
+        return result > 0;
+    }
+}
