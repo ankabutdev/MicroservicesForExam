33c4260c9593cad2b310ebbe594446ef42c0c0b3
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:40:59 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/StudentCase/Queries/SearchStudentByFullNameQuery.cs
@@ -0,0 +1,9 @@
+﻿using Kindergarten.Domain.Entities.Students;
+using MediatR;
+
+namespace Kindergarten.Application.UseCases.StudentCase.Queries;
+
+public class SearchStudentByFullNameQuery : IRequest<IQueryable<Student>>
+{
+    public string FullName { get; set; }
+}
