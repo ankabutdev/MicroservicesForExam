042e89bc265ed7425f7a431ab209f87d20e6b27f
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:41:25 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Domain/Dtos/Employees/EmployeeUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using Kindergarten.Domain.Enums;
+
+namespace Kindergarten.Domain.Dtos.Employees;
+
+public class EmployeeUpdateDto
+{
+    public long Id { get; set; }
+
+    public string FullName { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public Gender Gender { get; set; }
+}
