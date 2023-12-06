f3429c15233f2f552c249205996b9597b7940122
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:39:20 +0500

    Improve

---
+++ src/Nikoh/Nikoh.Application/UseCases/RequirementCases/Commands/CreateReqCmd.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Nikoh.Application.UseCases.RequirementCases.Commands;
+
+public class CreateReqCmd : IRequest<bool>
+{
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+}
