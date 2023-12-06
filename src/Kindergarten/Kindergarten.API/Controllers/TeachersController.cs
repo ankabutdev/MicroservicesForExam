5fb17721881d23a047327bdfb1078630ab6b5297
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 18:26:41 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.API/Controllers/TeachersController.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using Kindergarten.Application.UseCases.TeacherCase.Commands;
+using Kindergarten.Application.UseCases.TeacherCase.Queries;
+using Kindergarten.Domain.Dtos.Teachers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kindergarten.API.Controllers;
+
+[Route("api/teachers")]
+[ApiController]
+public class TeachersController : ControllerBase
+{
+    private readonly IMapper _mapper;
+    private readonly IMediator _mediator;
+
+    public TeachersController(IMapper mapper, IMediator mediator)
+    {
+        _mapper = mapper;
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var result = await _mediator
+            .Send(new GetAllTeacherQuery());
+
+        return Ok(result);
+    }
+
+    [HttpGet("{Id}")]
+    public async Task<IActionResult> GetByIdAsync(long Id)
+    {
+        var result = await _mediator
+            .Send(new GetByIdTeacherQuery() { Id = Id });
+
+        return Ok(result);
+    }
+
+    [HttpGet("fullName")]
+    public async Task<IActionResult> SearchByGroupNameAsnyc(string fullName)
+    {
+        var result = await _mediator
+            .Send(new SearchTeacherByFullNameQuery() { FullName = fullName });
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsnyc(CreateTeacherDto dto)
+    {
+        var teacher = _mapper.Map<CreateTeacherCmd>(dto);
+
+        var result = await _mediator.Send(teacher);
+
+        return Ok(result);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateAsync(UpdateTeacherDto dto)
+    {
+        var teacher = _mapper.Map<UpdateTeacherCmd>(dto);
+
+        var result = await _mediator.Send(teacher);
+
+        return Ok(result);
+    }
+
+    [HttpDelete("{Id}")]
+    public async Task<IActionResult> DeleteAsync(long Id)
+    {
+        var result = await _mediator
+            .Send(new DeleteTeacherCmd() { Id = Id });
+
+        return Ok(result);
+    }
+}
