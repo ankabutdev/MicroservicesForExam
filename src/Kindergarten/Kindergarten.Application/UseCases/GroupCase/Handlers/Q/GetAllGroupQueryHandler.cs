dd67f4f34d500ac3b27acb16a6a5db3edf0c5c21
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:38:54 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/GroupCase/Handlers/Q/GetAllGroupQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Kindergarten.Application.Abstractions;
+using Kindergarten.Application.UseCases.GroupCase.Queries;
+using Kindergarten.Domain.Entities.Groups;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kindergarten.Application.UseCases.GroupCase.Handlers.Q;
+
+public class GetAllGroupQueryHandler : IRequestHandler<GetAllGroupQuery, IEnumerable<Group>>
+{
+    private readonly IAppDbContext _context;
+
+    public GetAllGroupQueryHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Group>> Handle(GetAllGroupQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Groups.ToListAsync(cancellationToken);
+    }
+}
