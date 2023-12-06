719fe3f3b78abfff0ddac17064b1f96703795c44
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:37:06 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/AdminCase/Handlers/Q/GetAllAdminQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using Kindergarten.Application.Abstractions;
+using Kindergarten.Application.UseCases.AdminCase.Queries;
+using Kindergarten.Domain.Entities.Admins;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kindergarten.Application.UseCases.AdminCase.Handlers.Q;
+
+public class GetAllAdminQueryHandler : IRequestHandler<GetAllAdminQuery, IEnumerable<Admin>>
+{
+    private readonly IAppDbContext _context;
+
+    public GetAllAdminQueryHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Admin>> Handle(GetAllAdminQuery request,
+        CancellationToken cancellationToken)
+    {
+        return await _context.Admins.ToListAsync(cancellationToken);
+    }
+}
