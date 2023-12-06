92a1ca66dcb6d0f43dfc4157f4755b62931ebe09
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:41:54 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Domain/Entities/Employees/Employee.cs
@@ -0,0 +1,15 @@
+﻿using Kindergarten.Domain.Enums;
+
+namespace Kindergarten.Domain.Entities.Employees;
+
+public class Employee : BaseEntity
+{
+    public string FullName { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public Gender Gender { get; set; }
+
+}
