3f43fe5fda459689f4e846ab5633489dc388315b
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:41:40 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Domain/Dtos/Parents/UpdateParentDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Kindergarten.Domain.Dtos.Parents;
+
+public class UpdateParentDto
+{
+    public long Id { get; set; }
+
+    public string MotherFullName { get; set; }
+
+    public string FatherFullName { get; set; }
+
+    public string PassportSeriaNumber { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string Email { get; set; }
+
+    public string Address { get; set; }
+}
