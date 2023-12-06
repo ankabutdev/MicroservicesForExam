032139c6ccc5ae8a28d2e35abae0c783d2ca26a5
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:39:58 +0500

    Improve

--- src/Nikoh/Nikoh.Application/DependecyInjection.cs
+++ src/Nikoh/Nikoh.Application/DependecyInjection.cs
@@ -1,6 +1,16 @@
-﻿namespace Nikoh.Application;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Nikoh.Application.Mappers;
+using System.Reflection;
 
-public class DependecyInjection
+namespace Nikoh.Application;
+
+public static class DepandecyInjection
 {
-
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddAutoMapper(typeof(MappingConfiguration));
+        return services;
+    }
 }
