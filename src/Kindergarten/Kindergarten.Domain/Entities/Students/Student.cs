805cead3ef2468749f2fd3fa3353d2e22d2763ec
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:42:05 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Domain/Entities/Students/Student.cs
@@ -0,0 +1,26 @@
+﻿using Kindergarten.Domain.Entities.Groups;
+using Kindergarten.Domain.Entities.Parents;
+using Kindergarten.Domain.Enums;
+
+namespace Kindergarten.Domain.Entities.Students;
+
+public class Student : BaseEntity
+{
+    public string FullName { get; set; }
+
+    public Gender Gender { get; set; }
+
+    public DateTime RegisteredAt { get; set; }
+
+    public string Address { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+
+    public long ParentId { get; set; }
+
+    public Parent Parent { get; set; }
+
+    public long GroupId { get; set; }
+
+    public Group Group { get; set; }
+}
