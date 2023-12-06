17604824c3a95235286c89b92b267dd042717541
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:40:09 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/StudentCase/Commands/CreateStudentCmd.cs
@@ -0,0 +1,22 @@
+﻿using Kindergarten.Domain.Enums;
+using MediatR;
+
+namespace Kindergarten.Application.UseCases.StudentCase.Commands;
+
+public class CreateStudentCmd : IRequest<bool>
+{
+    public string FullName { get; set; }
+
+    public Gender Gender { get; set; }
+
+    public DateTime RegisteredAt { get; set; }
+
+    public string Address { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+
+    public long ParentId { get; set; }
+
+    public long GroupId { get; set; }
+
+}
