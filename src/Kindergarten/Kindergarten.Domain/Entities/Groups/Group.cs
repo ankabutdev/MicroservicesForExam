79cd92c07d49e26231ea5e124cddc6838a264730
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:41:58 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Domain/Entities/Groups/Group.cs
@@ -0,0 +1,22 @@
+﻿using Kindergarten.Domain.Entities.Students;
+using Kindergarten.Domain.Entities.Teachers;
+
+namespace Kindergarten.Domain.Entities.Groups;
+
+public class Group : BaseEntity
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public long AgeGroup { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+
+    public ICollection<Student> Students { get; set; }
+
+    public ICollection<Teacher> Teachers { get; set; }
+
+}
