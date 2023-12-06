b0a566403bd7e6bbd9f880b79da6f75b31c8c2e2
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:39:40 +0500

    Improve

---
+++ src/Nikoh/Nikoh.Application/UseCases/RequirementCases/Handlers/Q/GetAllReqQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Nikoh.Application.Abstractions;
+using Nikoh.Application.UseCases.RequirementCases.Queris;
+using NIkoh.Domain.Entities.Requirements;
+
+namespace Nikoh.Application.UseCases.RequirementCases.Handlers.Q;
+
+public class GetAllReqQueryHandler : IRequestHandler<GetAllReqQuery, IEnumerable<Requirement>>
+{
+    private readonly IAppDbContext _context;
+
+    public GetAllReqQueryHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Requirement>> Handle(GetAllReqQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Requirements.ToListAsync(cancellationToken);
+    }
+}
