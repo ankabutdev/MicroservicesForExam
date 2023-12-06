463bd7432a613dad87d544c0c8849e10f8d3453d
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:39:53 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/ParentCase/Handlers/C/UpdateParentCmdHandler.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Kindergarten.Application.Abstractions;
+using Kindergarten.Application.UseCases.ParentCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kindergarten.Application.UseCases.ParentCase.Handlers.C;
+
+public class UpdateParentCmdHandler : IRequestHandler<UpdateParentCmd, bool>
+{
+    private readonly IAppDbContext _context;
+    private readonly IMapper _mapper;
+
+    public UpdateParentCmdHandler(IAppDbContext context,
+        IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<bool> Handle(UpdateParentCmd request,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            var parent = await _context.Parents
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (parent is null)
+                throw new ArgumentNullException(nameof(parent));
+
+            _mapper.Map(request, parent);
+
+            _context.Parents.Update(parent);
+
+            var result = await _context
+                .SaveChangesAsync(cancellationToken);
+
+            return result > 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
