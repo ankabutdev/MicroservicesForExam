e28be100ce1955ee4581366d833240dd5947416d
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 18:27:19 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/TeacherCase/Handlers/C/UpdateTeacherCmdHandler.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Kindergarten.Application.Abstractions;
+using Kindergarten.Application.UseCases.TeacherCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kindergarten.Application.UseCases.TeacherCase.Handlers.C;
+
+public class UpdateTeacherCmdHandler : IRequestHandler<UpdateTeacherCmd, bool>
+{
+    private readonly IAppDbContext _context;
+    private readonly IMapper _mapper;
+
+    public UpdateTeacherCmdHandler(IAppDbContext context,
+        IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<bool> Handle(UpdateTeacherCmd request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var teacher = await _context.Teachers
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (teacher is null)
+                throw new ArgumentNullException(nameof(teacher));
+
+            _mapper.Map(request, teacher);
+
+            _context.Teachers.Update(teacher);
+
+            var result = await _context
+                .SaveChangesAsync(cancellationToken);
+
+            return result > 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
---
+++ src/Kindergarten/Kindergarten.Application/UseCases/TeacherCase/Handlers/Q/GetAllTeacherQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Kindergarten.Application.Abstractions;
+using Kindergarten.Application.UseCases.TeacherCase.Queries;
+using Kindergarten.Domain.Entities.Teachers;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kindergarten.Application.UseCases.TeacherCase.Handlers.Q;
+
+public class GetAllTeacherQueryHandler :
+    IRequestHandler<GetAllTeacherQuery, IEnumerable<Teacher>>
+{
+    private readonly IAppDbContext _context;
+
+    public GetAllTeacherQueryHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Teacher>> Handle(GetAllTeacherQuery request,
+        CancellationToken cancellationToken)
+    {
+        return await _context.Teachers
+            .Include(x => x.Groups)
+            .ToListAsync(cancellationToken);
+    }
+}
