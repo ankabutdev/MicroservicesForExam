26280f3c3b544ce827633313c8c8070fd78a07fb
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:38:22 +0500

    Improve

---
+++ src/Nikoh/Nikoh.Application/UseCases/MarriageCases/Handlers/C/CreateMarriageCmdHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using MediatR;
+using Nikoh.Application.Abstractions;
+using Nikoh.Application.UseCases.MarriageCases.Commands;
+using NIkoh.Domain.Entities.Marriages;
+
+namespace Nikoh.Application.UseCases.MarriageCases.Handlers.C;
+
+public class CreateMarriageCmdHandler : IRequestHandler<CreateMarriageCmd, bool>
+{
+    private readonly IAppDbContext _context;
+    private readonly IMapper _mapper;
+
+    public CreateMarriageCmdHandler(IAppDbContext context,
+        IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<bool> Handle(CreateMarriageCmd request,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            var entity = _mapper.Map<Marriage>(request);
+
+            await _context.Marriages.AddAsync(entity);
+            var result = await _context.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
