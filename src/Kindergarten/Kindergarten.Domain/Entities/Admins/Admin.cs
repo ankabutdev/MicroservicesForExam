6dbbfac503d231f3f7c9cf3c565a119b275753f0
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:41:50 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Domain/Entities/Admins/Admin.cs
@@ -0,0 +1,10 @@
+﻿namespace Kindergarten.Domain.Entities.Admins;
+
+public class Admin : BaseEntity
+{
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+}
