147eadb3ae124ef5f46f05375ab8e329227d60ff
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:40:00 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/ParentCase/Handlers/Q/SearchParentByPhoneNumberQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Kindergarten.Application.Abstractions;
+using Kindergarten.Application.UseCases.ParentCase.Queries;
+using Kindergarten.Domain.Entities.Parents;
+using MediatR;
+
+namespace Kindergarten.Application.UseCases.ParentCase.Handlers.Q;
+
+public class SearchParentByPhoneNumberQueryHandler :
+    IRequestHandler<SearchParentByPhoneNumberQuery, IQueryable<Parent>>
+{
+    private readonly IAppDbContext _context;
+
+    public SearchParentByPhoneNumberQueryHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IQueryable<Parent>> Handle(SearchParentByPhoneNumberQuery request,
+        CancellationToken cancellationToken)
+    {
+        var result = _context.Parents
+            .Where(x => x.PhoneNumber.ToLower()
+            .Contains(request.PhoneNumber.ToLower()));
+
+        return result;
+    }
+}
