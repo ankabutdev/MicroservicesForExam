2a22585628815c4993a440d98b768bf55c31e120
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:40:25 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/StudentCase/Handlers/C/DeleteStudentCmdHandler.cs
@@ -0,0 +1,34 @@
+﻿using Kindergarten.Application.Abstractions;
+using Kindergarten.Application.UseCases.StudentCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kindergarten.Application.UseCases.StudentCase.Handlers.C;
+
+public class DeleteStudentCmdHandler : IRequestHandler<DeleteStudentCmd, bool>
+{
+    private readonly IAppDbContext _context;
+
+    public DeleteStudentCmdHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> Handle(DeleteStudentCmd request, CancellationToken cancellationToken)
+    {
+        if (request.Id <= 0)
+            return false;
+
+        var student = await _context.Students
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+        if (student == null)
+            return false;
+        _context.Students.Remove(student);
+
+        var result = await _context
+            .SaveChangesAsync(cancellationToken);
+
+        return result > 0;
+    }
+}
