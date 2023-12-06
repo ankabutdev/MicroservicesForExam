40ac0522a6aa2d2f51648918811bc3504ad8adae
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:38:16 +0500

    Improve

---
+++ src/Nikoh/Nikoh.Application/UseCases/MarriageCases/Commands/UpdateMarriageCmd.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace Nikoh.Application.UseCases.MarriageCases.Commands;
+
+public class UpdateMarriageCmd : IRequest<bool>
+{
+    public long Id { get; set; }
+
+    public string Description { get; set; }
+
+    public string Healthy { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public long WomenId { get; set; }
+
+    public long ManId { get; set; }
+
+    public long RequirementId { get; set; }
+
+}
