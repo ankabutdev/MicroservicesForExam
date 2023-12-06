3d23659ffeae7eb107d869a5de063247cb7d64b7
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 18:27:50 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/TeacherCase/Queries/GetByIdTeacherQuery.cs
@@ -0,0 +1,9 @@
+﻿using Kindergarten.Domain.Entities.Teachers;
+using MediatR;
+
+namespace Kindergarten.Application.UseCases.TeacherCase.Queries;
+
+public class GetByIdTeacherQuery : IRequest<Teacher>
+{
+    public long Id { get; set; }
+}
---
+++ src/Kindergarten/Kindergarten.Application/UseCases/TeacherCase/Queries/SearchTeacherByFullNameQuery.cs
@@ -0,0 +1,9 @@
+﻿using Kindergarten.Domain.Entities.Teachers;
+using MediatR;
+
+namespace Kindergarten.Application.UseCases.TeacherCase.Queries;
+
+public class SearchTeacherByFullNameQuery : IRequest<IQueryable<Teacher>>
+{
+    public string FullName { get; set; }
+}
