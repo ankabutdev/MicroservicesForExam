c5db11561a64b8755974722947d6b91c4bc04d60
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 18:27:35 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/TeacherCase/Handlers/Q/GetByIdTeacherQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Kindergarten.Application.Abstractions;
+using Kindergarten.Application.UseCases.TeacherCase.Queries;
+using Kindergarten.Domain.Entities.Teachers;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kindergarten.Application.UseCases.TeacherCase.Handlers.Q;
+
+public class GetByIdTeacherQueryHandler : IRequestHandler<GetByIdTeacherQuery, Teacher>
+{
+    private readonly IAppDbContext _context;
+
+    public GetByIdTeacherQueryHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Teacher> Handle(GetByIdTeacherQuery request, CancellationToken cancellationToken)
+    {
+        var result = await _context.Teachers
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+        return result ?? throw new Exception("Teacher not found!");
+    }
+}
