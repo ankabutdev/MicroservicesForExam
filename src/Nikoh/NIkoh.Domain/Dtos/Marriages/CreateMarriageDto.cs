fc393e0dd1f36f3190ae7a9905c92e82b70e2f98
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:40:07 +0500

    Improve

---
+++ src/Nikoh/NIkoh.Domain/Dtos/Marriages/CreateMarriageDto.cs
@@ -0,0 +1,17 @@
+﻿namespace NIkoh.Domain.Dtos.Marriages;
+
+public class CreateMarriageDto
+{
+    public string Description { get; set; }
+
+    public string Healthy { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public long WomenId { get; set; }
+
+    public long ManId { get; set; }
+
+    public long RequirementId { get; set; }
+
+}
