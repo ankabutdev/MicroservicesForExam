4c0457b9fad453743f46646812eb14b293f7027b
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:39:13 +0500

    Improve

---
+++ src/Nikoh/Nikoh.Application/UseCases/PersonCases/Handlers/Q/GetAllPersonQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Nikoh.Application.Abstractions;
+using Nikoh.Application.UseCases.PersonCases.Queris;
+using NIkoh.Domain.Entities.Persons;
+
+namespace Nikoh.Application.UseCases.PersonCases.Handlers.Q;
+
+public class GetAllPersonQueryHandler :
+    IRequestHandler<GetAllPersonQuery, IEnumerable<Person>>
+{
+    private readonly IAppDbContext _context;
+
+    public GetAllPersonQueryHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Person>> Handle(GetAllPersonQuery request,
+        CancellationToken cancellationToken)
+    {
+        return await _context.People.ToListAsync(cancellationToken);
+    }
+}
---
+++ src/Nikoh/Nikoh.Application/UseCases/PersonCases/Handlers/Q/GetByIdPersonQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Nikoh.Application.Abstractions;
+using Nikoh.Application.UseCases.PersonCases.Queris;
+using NIkoh.Domain.Entities.Persons;
+
+namespace Nikoh.Application.UseCases.PersonCases.Handlers.Q;
+
+public class GetByIdPersonQueryHandler : IRequestHandler<GetByIdPersonQuery, Person>
+{
+    private readonly IAppDbContext _context;
+
+    public GetByIdPersonQueryHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Person> Handle(GetByIdPersonQuery request,
+        CancellationToken cancellationToken)
+    {
+        var result = await _context.People
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+        return result ?? throw new Exception("Person not found!");
+    }
+}
