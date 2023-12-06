c50af9a873620ceb29791262a7488757bd9fe3a4
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:40:32 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/StudentCase/Handlers/Q/GetAllStudentQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Kindergarten.Application.Abstractions;
+using Kindergarten.Application.UseCases.StudentCase.Queries;
+using Kindergarten.Domain.Entities.Students;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kindergarten.Application.UseCases.StudentCase.Handlers.Q;
+
+public class GetAllStudentQueryHandler : IRequestHandler<GetAllStudentQuery, IEnumerable<Student>>
+{
+    private readonly IAppDbContext _context;
+
+    public GetAllStudentQueryHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Student>> Handle(GetAllStudentQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Students.ToListAsync(cancellationToken);
+    }
+}
