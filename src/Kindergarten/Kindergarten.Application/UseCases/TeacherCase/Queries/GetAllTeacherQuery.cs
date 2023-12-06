9ef5cfcbecd19e22c61f8da376a36f330945c501
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 18:27:44 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/TeacherCase/Queries/GetAllTeacherQuery.cs
@@ -0,0 +1,8 @@
+﻿using Kindergarten.Domain.Entities.Teachers;
+using MediatR;
+
+namespace Kindergarten.Application.UseCases.TeacherCase.Queries;
+
+public class GetAllTeacherQuery : IRequest<IEnumerable<Teacher>>
+{
+}
