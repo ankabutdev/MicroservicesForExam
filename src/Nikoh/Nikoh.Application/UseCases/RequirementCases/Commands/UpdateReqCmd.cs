2ca64aa7dc92033f117702c8810be7204fb65795
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:39:27 +0500

    Improve

---
+++ src/Nikoh/Nikoh.Application/UseCases/RequirementCases/Commands/UpdateReqCmd.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Nikoh.Application.UseCases.RequirementCases.Commands;
+
+public class UpdateReqCmd : IRequest<bool>
+{
+    public long Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+}
