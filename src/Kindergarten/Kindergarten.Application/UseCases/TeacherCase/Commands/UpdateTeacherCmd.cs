da1334829e5abbf7e98fb4928b45631541fcc4f5
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 18:27:05 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/TeacherCase/Commands/UpdateTeacherCmd.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace Kindergarten.Application.UseCases.TeacherCase.Commands;
+
+public class UpdateTeacherCmd : IRequest<bool>
+{
+    public long Id { get; set; }
+
+    public string FullName { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string Address { get; set; }
+
+}
