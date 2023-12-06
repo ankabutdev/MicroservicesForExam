3ff3a2ab54d74ff8d2928f890da887d83a0c2769
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:37:49 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/EmpoloyeeCase/Handlers/Q/GetAllEmpQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Kindergarten.Application.Abstractions;
+using Kindergarten.Application.UseCases.EmpoloyeeCase.Queries;
+using Kindergarten.Domain.Entities.Employees;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kindergarten.Application.UseCases.EmpoloyeeCase.Handlers.Q;
+
+public class GetAllEmpQueryHandler : IRequestHandler<GetAllEmpQuery, IEnumerable<Employee>>
+{
+    private readonly IAppDbContext _context;
+
+    public GetAllEmpQueryHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Employee>> Handle(GetAllEmpQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Employees.ToListAsync(cancellationToken);
+    }
+}
