17a6dc6c1639ed1e99bdad0800c448c57bf366a7
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:39:56 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/ParentCase/Handlers/Q/GetAllParentQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using Kindergarten.Application.Abstractions;
+using Kindergarten.Application.UseCases.ParentCase.Queries;
+using Kindergarten.Domain.Entities.Parents;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kindergarten.Application.UseCases.ParentCase.Handlers.Q;
+
+public class GetAllParentQueryHandler : IRequestHandler<GetAllParentQuery, IEnumerable<Parent>>
+{
+    private readonly IAppDbContext _context;
+
+    public GetAllParentQueryHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Parent>> Handle(GetAllParentQuery request,
+        CancellationToken cancellationToken)
+    {
+        return await _context.Parents.ToListAsync(cancellationToken);
+    }
+}
