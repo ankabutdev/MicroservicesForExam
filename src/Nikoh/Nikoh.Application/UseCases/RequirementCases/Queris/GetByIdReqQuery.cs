0f411be731d68a2376c948cab566b794e9831abf
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:39:53 +0500

    Improve

---
+++ src/Nikoh/Nikoh.Application/UseCases/RequirementCases/Queris/GetByIdReqQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using NIkoh.Domain.Entities.Requirements;
+
+namespace Nikoh.Application.UseCases.RequirementCases.Queris;
+
+public class GetByIdReqQuery : IRequest<Requirement>
+{
+    public long Id { get; set; }
+}
