c917697a2433aa7eb0650e23eb1c011db107a5c2
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:36:52 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/AdminCase/Commands/UpdateAdminCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Kindergarten.Application.UseCases.AdminCases.Commands;
+
+public class UpdateAdminCommand : IRequest<bool>
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+}
