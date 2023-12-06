d0a2c2f678a9749a94f4b7c4b275d93f4943eecc
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:39:04 +0500

    Improve

---
+++ src/Nikoh/Nikoh.Application/UseCases/PersonCases/Handlers/C/DeletePersonCmdHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Nikoh.Application.Abstractions;
+using Nikoh.Application.UseCases.PersonCases.Commands;
+
+namespace Nikoh.Application.UseCases.PersonCases.Handlers.C;
+
+public class DeletePersonCmdHandler : IRequestHandler<DeletePersonCmd, bool>
+{
+    private readonly IAppDbContext _context;
+
+    public DeletePersonCmdHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> Handle(DeletePersonCmd request, CancellationToken cancellationToken)
+    {
+        if (request.Id <= 0)
+            return false;
+
+        var people = await _context.People
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+        if (people == null)
+            return false;
+
+        _context.People.Remove(people);
+
+        var result = await _context
+            .SaveChangesAsync(cancellationToken);
+
+        return result > 0;
+    }
+}
---
+++ src/Nikoh/Nikoh.Application/UseCases/PersonCases/Handlers/C/UpdatePersonCmdHandler.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Nikoh.Application.Abstractions;
+using Nikoh.Application.UseCases.PersonCases.Commands;
+
+namespace Nikoh.Application.UseCases.PersonCases.Handlers.C;
+
+public class UpdatePersonCmdHandler : IRequestHandler<UpdatePersonCmd, bool>
+{
+    private readonly IAppDbContext _context;
+    private readonly IMapper _mapper;
+
+    public UpdatePersonCmdHandler(IAppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<bool> Handle(UpdatePersonCmd request,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            var people = await _context.People
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (people is null)
+                throw new ArgumentNullException(nameof(people));
+
+            _mapper.Map(request, people);
+
+            _context.People.Update(people);
+
+            var result = await _context
+                .SaveChangesAsync(cancellationToken);
+
+            return result > 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
