6fc1dd5f1a2126820187fdfd89c383f4c46b5d3d
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:36:45 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/AdminCase/Commands/CreateAdminCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Kindergarten.Application.UseCases.AdminCases.Commands;
+
+public class CreateAdminCommand : IRequest<bool>
+{
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+}
