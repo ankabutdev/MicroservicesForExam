6a15202917e5c3081f603a3243eb0ed600d7cbc1
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:40:30 +0500

    Improve

---
+++ src/Nikoh/NIkoh.Domain/Entities/Marriages/Marriage.cs
@@ -0,0 +1,27 @@
+﻿using NIkoh.Domain.Entities.Persons;
+using NIkoh.Domain.Entities.Requirements;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NIkoh.Domain.Entities.Marriages;
+
+public class Marriage : BaseEntity
+{
+    public string Description { get; set; }
+
+    public string Healthy { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public long WomenId { get; set; }
+
+    [ForeignKey(nameof(WomenId))]
+    public Person? Women { get; set; }
+
+    public long ManId { get; set; }
+
+    [ForeignKey(nameof(ManId))]
+    public Person? Man { get; set; }
+
+    public long RequirementId { get; set; }
+    public Requirement Requirements { get; set; }
+}
