4db6b68ee28e258bb62f710f5a9d1c79519e0a87
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:38:41 +0500

    Improve

---
+++ src/Nikoh/Nikoh.Application/UseCases/MarriageCases/Queris/GetAllMarriageQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using NIkoh.Domain.Entities.Marriages;
+
+namespace Nikoh.Application.UseCases.MarriageCases.Queris;
+
+public class GetAllMarriageQuery : IRequest<IEnumerable<Marriage>>
+{
+}
