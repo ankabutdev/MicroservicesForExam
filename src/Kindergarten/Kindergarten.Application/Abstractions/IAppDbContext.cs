85855a693acb967edb1b60d05f121f1b06795e1b
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:36:36 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/Abstractions/IAppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Kindergarten.Domain.Entities.Admins;
+using Kindergarten.Domain.Entities.Employees;
+using Kindergarten.Domain.Entities.Groups;
+using Kindergarten.Domain.Entities.Parents;
+using Kindergarten.Domain.Entities.Students;
+using Kindergarten.Domain.Entities.Teachers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kindergarten.Application.Abstractions;
+
+public interface IAppDbContext
+{
+    DbSet<Admin> Admins { get; set; }
+
+    DbSet<Employee> Employees { get; set; }
+
+    DbSet<Student> Students { get; set; }
+
+    DbSet<Group> Groups { get; set; }
+
+    DbSet<Teacher> Teachers { get; set; }
+
+    DbSet<Parent> Parents { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
