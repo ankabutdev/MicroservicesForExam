3cff23ad17da8458e2bcb2b05bfc7687113baafd
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 18:27:41 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/TeacherCase/Handlers/Q/SearchTeacherByFullNameQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Kindergarten.Application.Abstractions;
+using Kindergarten.Application.UseCases.TeacherCase.Queries;
+using Kindergarten.Domain.Entities.Teachers;
+using MediatR;
+
+namespace Kindergarten.Application.UseCases.TeacherCase.Handlers.Q;
+
+public class SearchTeacherByFullNameQueryHandler :
+    IRequestHandler<SearchTeacherByFullNameQuery, IQueryable<Teacher>>
+{
+    private readonly IAppDbContext _context;
+
+    public SearchTeacherByFullNameQueryHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IQueryable<Teacher>> Handle(SearchTeacherByFullNameQuery request,
+        CancellationToken cancellationToken)
+    {
+        var result = _context.Teachers
+           .Where(x => x.FullName.ToLower()
+           .Contains(request.FullName.ToLower()));
+
+        return result;
+    }
+}
