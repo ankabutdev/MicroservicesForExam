86dc7014011d6dd8c13e901cf29dc6f1a294dbe6
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:41:14 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Domain/Dtos/Admins/CreateAdminDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Kindergarten.Domain.Dtos.Admins;
+
+public class CreateAdminDto
+{
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+}
