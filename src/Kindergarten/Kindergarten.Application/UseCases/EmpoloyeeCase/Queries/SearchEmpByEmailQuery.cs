048dad54c55e2ef4fd658d9a2e989ac0c449501a
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:38:17 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/EmpoloyeeCase/Queries/SearchEmpByEmailQuery.cs
@@ -0,0 +1,9 @@
+﻿using Kindergarten.Domain.Entities.Employees;
+using MediatR;
+
+namespace Kindergarten.Application.UseCases.EmpoloyeeCase.Queries;
+
+public class SearchEmpByEmailQuery : IRequest<IQueryable<Employee>>
+{
+    public string Email { get; set; }
+}
