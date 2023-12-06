7e2ec8b8b4cc4bec232091ae21f7c567a960265a
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:38:26 +0500

    Improve

---
+++ src/Nikoh/Nikoh.Application/UseCases/MarriageCases/Handlers/C/DeleteMarriageCmdHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Nikoh.Application.Abstractions;
+using Nikoh.Application.UseCases.MarriageCases.Commands;
+
+namespace Nikoh.Application.UseCases.MarriageCases.Handlers.C;
+
+public class DeleteMarriageCmdHandler : IRequestHandler<DeleteMarriageCmd, bool>
+{
+    private readonly IAppDbContext _context;
+
+    public DeleteMarriageCmdHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> Handle(DeleteMarriageCmd request, CancellationToken cancellationToken)
+    {
+        if (request.Id <= 0)
+            return false;
+
+        var marriage = await _context.Marriages
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+        if (marriage == null)
+            return false;
+
+        _context.Marriages.Remove(marriage);
+
+        var result = await _context
+            .SaveChangesAsync(cancellationToken);
+
+        return result > 0;
+    }
+}
