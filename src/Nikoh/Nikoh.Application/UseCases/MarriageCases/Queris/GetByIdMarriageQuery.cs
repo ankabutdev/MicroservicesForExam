d2a645bec23c09343c66c853f404745d9baa817c
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:38:45 +0500

    Improve

---
+++ src/Nikoh/Nikoh.Application/UseCases/MarriageCases/Queris/GetByIdMarriageQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using NIkoh.Domain.Entities.Marriages;
+
+namespace Nikoh.Application.UseCases.MarriageCases.Queris;
+
+public class GetByIdMarriageQuery : IRequest<Marriage>
+{
+    public long Id { get; set; }
+}
