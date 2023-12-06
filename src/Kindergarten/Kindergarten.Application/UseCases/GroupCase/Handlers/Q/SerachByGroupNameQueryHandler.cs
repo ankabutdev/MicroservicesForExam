0bbc3d71998249daea9ee46fb34dfce81be0c873
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:39:04 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/GroupCase/Handlers/Q/SerachByGroupNameQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Kindergarten.Application.Abstractions;
+using Kindergarten.Application.UseCases.GroupCase.Queries;
+using Kindergarten.Domain.Entities.Groups;
+using MediatR;
+
+namespace Kindergarten.Application.UseCases.GroupCase.Handlers.Q;
+
+public class SerachByGroupNameQueryHandler : IRequestHandler<SearchByGroupNameQuery, IQueryable<Group>>
+{
+    private readonly IAppDbContext _context;
+
+    public SerachByGroupNameQueryHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IQueryable<Group>> Handle(SearchByGroupNameQuery request,
+        CancellationToken cancellationToken)
+    {
+        var result = _context.Groups
+            .Where(x => x.Name.ToLower()
+            .Contains(request.Name.ToLower()));
+
+        return result;
+    }
+}
