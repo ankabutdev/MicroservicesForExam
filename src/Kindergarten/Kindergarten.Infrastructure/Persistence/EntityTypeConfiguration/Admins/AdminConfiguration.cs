8000fb245b851c2ab3b1211e761d1ca51dcb7ec7
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:42:36 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Infrastructure/Persistence/EntityTypeConfiguration/Admins/AdminConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Kindergarten.Domain.Entities.Admins;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Kindergarten.Infrastructure.Persistence.EntityTypeConfiguration.Admins;
+
+public class AdminConfiguration : IEntityTypeConfiguration<Admin>
+{
+    public void Configure(EntityTypeBuilder<Admin> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .UseIdentityColumn();
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(256)
+            .IsRequired();
+
+        builder.Property(x => x.Password)
+            .HasMaxLength(256)
+            .IsRequired();
+    }
+}
