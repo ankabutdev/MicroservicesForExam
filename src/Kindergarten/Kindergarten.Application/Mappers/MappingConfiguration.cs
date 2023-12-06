05c9cea22426b4b73d8a7c171961255777059e29
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 18:26:50 +0500

    added mapping

--- src/Kindergarten/Kindergarten.Application/Mappers/MappingConfiguration.cs
+++ src/Kindergarten/Kindergarten.Application/Mappers/MappingConfiguration.cs
@@ -4,16 +4,19 @@ using Kindergarten.Application.UseCases.EmpoloyeeCase.Commands;
 using Kindergarten.Application.UseCases.GroupCase.Commands;
 using Kindergarten.Application.UseCases.ParentCase.Commands;
 using Kindergarten.Application.UseCases.StudentCase.Commands;
+using Kindergarten.Application.UseCases.TeacherCase.Commands;
 using Kindergarten.Domain.Dtos.Admins;
 using Kindergarten.Domain.Dtos.Employees;
 using Kindergarten.Domain.Dtos.Groups;
 using Kindergarten.Domain.Dtos.Parents;
 using Kindergarten.Domain.Dtos.Students;
+using Kindergarten.Domain.Dtos.Teachers;
 using Kindergarten.Domain.Entities.Admins;
 using Kindergarten.Domain.Entities.Employees;
 using Kindergarten.Domain.Entities.Groups;
 using Kindergarten.Domain.Entities.Parents;
 using Kindergarten.Domain.Entities.Students;
+using Kindergarten.Domain.Entities.Teachers;
 
 namespace Kindergarten.Application.Mappers;
 
@@ -55,5 +58,12 @@ public class MappingConfiguration : Profile
 
         CreateMap<CreateStudentCmd, CreateStudnetDto>().ReverseMap();
         CreateMap<UpdateStudentCmd, UpdateStudentDto>().ReverseMap();
+
+        // Teachers
+        CreateMap<Teacher, CreateTeacherCmd>().ReverseMap();
+        CreateMap<Teacher, UpdateTeacherCmd>().ReverseMap();
+
+        CreateMap<CreateTeacherCmd, CreateTeacherDto>().ReverseMap();
+        CreateMap<UpdateTeacherCmd, UpdateTeacherDto>().ReverseMap();
     }
 }
