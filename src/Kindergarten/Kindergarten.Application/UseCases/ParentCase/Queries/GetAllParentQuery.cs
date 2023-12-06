24e185f114a700d59bf4191b6b207a4358a36381
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:40:04 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/ParentCase/Queries/GetAllParentQuery.cs
@@ -0,0 +1,7 @@
+﻿using Kindergarten.Domain.Entities.Parents;
+using MediatR;
+
+namespace Kindergarten.Application.UseCases.ParentCase.Queries;
+
+public class GetAllParentQuery : IRequest<IEnumerable<Parent>>
+{ }
