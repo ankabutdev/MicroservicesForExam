9fb778f5dbc287c9f162775d3a2fff2373c33e3a
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:37:27 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/EmpoloyeeCase/Commands/EmpCreateCmd.cs
@@ -0,0 +1,15 @@
+﻿using Kindergarten.Domain.Enums;
+using MediatR;
+
+namespace Kindergarten.Application.UseCases.EmpoloyeeCase.Commands;
+
+public class EmpCreateCmd : IRequest<bool>
+{
+    public string FullName { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public Gender Gender { get; set; }
+}
