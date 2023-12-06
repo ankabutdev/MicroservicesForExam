ea4695a3c457aeb60c0262226bcd4f41c9a822a8
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 18:27:56 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Domain/Dtos/Teachers/CreateTeacherDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Kindergarten.Domain.Dtos.Teachers;
+
+public class CreateTeacherDto
+{
+    public string FullName { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string Address { get; set; }
+}
---
+++ src/Kindergarten/Kindergarten.Domain/Dtos/Teachers/UpdateTeacherDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Kindergarten.Domain.Dtos.Teachers;
+
+public class UpdateTeacherDto
+{
+    public long Id { get; set; }
+
+    public string FullName { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string Address { get; set; }
+}
