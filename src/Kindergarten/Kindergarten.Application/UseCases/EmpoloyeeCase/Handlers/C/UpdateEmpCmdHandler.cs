5b11ee6c6308a4b527ef2100929dfcc03534ca7d
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:37:46 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/EmpoloyeeCase/Handlers/C/UpdateEmpCmdHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Kindergarten.Application.Abstractions;
+using Kindergarten.Application.UseCases.EmpoloyeeCase.Commands;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kindergarten.Application.UseCases.EmpoloyeeCase.Handlers.C;
+
+public class UpdateEmpCmdHandler : IRequestHandler<EmpUpdateCmd, bool>
+{
+    private readonly IAppDbContext _context;
+    private readonly IMapper _mapper;
+
+    public UpdateEmpCmdHandler(IAppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<bool> Handle(EmpUpdateCmd request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var emp = await _context.Employees
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (emp is null)
+                throw new ArgumentNullException(nameof(emp));
+
+            _mapper.Map(request, emp);
+
+            _context.Employees.Update(emp);
+
+            var result = await _context
+                .SaveChangesAsync(cancellationToken);
+
+            return result > 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
