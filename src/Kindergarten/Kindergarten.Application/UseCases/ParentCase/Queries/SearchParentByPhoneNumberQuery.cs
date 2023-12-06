3318011fab3f54e37d862ad938bc7f37adec854e
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:40:07 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/ParentCase/Queries/SearchParentByPhoneNumberQuery.cs
@@ -0,0 +1,9 @@
+﻿using Kindergarten.Domain.Entities.Parents;
+using MediatR;
+
+namespace Kindergarten.Application.UseCases.ParentCase.Queries;
+
+public class SearchParentByPhoneNumberQuery : IRequest<IQueryable<Parent>>
+{
+    public string PhoneNumber { get; set; }
+}
