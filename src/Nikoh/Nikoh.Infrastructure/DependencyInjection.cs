8692241e85ac5f9fe8bcd5fc2220e98ec1360953
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 22:41:28 +0500

    Nikoh Layer Some methodes improved

--- src/Nikoh/Nikoh.Infrastructure/DependencyInjection.cs
+++ src/Nikoh/Nikoh.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,21 @@
-﻿namespace Nikoh.Infrastructure;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nikoh.Application.Abstractions;
+using Nikoh.Infrastructure.Persitence;
 
-public class DependencyInjection
+namespace Nikoh.Infrastructure;
+
+public static class DependencyInjection
 {
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext <IAppDbContext, AppDbContext > (options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        });
 
+        return services;
+    }
 }
