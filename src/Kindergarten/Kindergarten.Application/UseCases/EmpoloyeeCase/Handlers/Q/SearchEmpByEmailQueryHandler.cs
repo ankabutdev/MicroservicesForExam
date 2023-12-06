728b9be875c3623636d6efdf3bf01b7ae3a036a1
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:37:59 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Application/UseCases/EmpoloyeeCase/Handlers/Q/SearchEmpByEmailQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Kindergarten.Application.Abstractions;
+using Kindergarten.Application.UseCases.EmpoloyeeCase.Queries;
+using Kindergarten.Domain.Entities.Employees;
+using MediatR;
+
+namespace Kindergarten.Application.UseCases.EmpoloyeeCase.Handlers.Q;
+
+public class SearchEmpByEmailQueryHandler : IRequestHandler<SearchEmpByEmailQuery, IQueryable<Employee>>
+{
+    private readonly IAppDbContext _context;
+
+    public SearchEmpByEmailQueryHandler(IAppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IQueryable<Employee>> Handle(SearchEmpByEmailQuery request,
+        CancellationToken cancellationToken)
+    {
+        var result = _context.Employees
+            .Where(x => x.Email.ToLower()
+            .Contains(request.Email.ToLower()));
+
+        return result;
+    }
+}
