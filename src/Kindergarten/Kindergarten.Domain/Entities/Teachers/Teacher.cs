d69387a13d3018e57cd53de0a01f5a36fcb65d47
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-06 17:42:08 +0500

    improve

---
+++ src/Kindergarten/Kindergarten.Domain/Entities/Teachers/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using Kindergarten.Domain.Entities.Groups;
+
+namespace Kindergarten.Domain.Entities.Teachers;
+
+public class Teacher : BaseEntity
+{
+    public string FullName { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string Address { get; set; }
+
+    public ICollection<Group> Groups { get; set; }
+
+}
