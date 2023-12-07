b7c1071b1e07012ee0ebe2973e6d7f1f108b141b
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 18:40:06 +0500

    Added Configuration

---
+++ src/Olx/Olx.Infrastructure/Persistence/EntityTypeConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Olx.Domain.Entities;
+
+namespace Olx.Infrastructure.Persistence.EntityTypeConfiguration;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Name).IsRequired();
+        builder.Property(c => c.Description);
+        builder.Property(c => c.CreatedAt).IsRequired();
+        builder.Property(c => c.UpdatedAt).IsRequired();
+
+        builder.HasMany(c => c.Announcements)
+            .WithOne(a => a.Category)
+            .HasForeignKey(a => a.CategortId)
+            .OnDelete(DeleteBehavior.SetNull);
+    }
+}
