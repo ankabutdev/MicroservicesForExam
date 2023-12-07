499aaf68a67e1764b307a0ee7fbc863901c98c3a
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 18:40:15 +0500

    Added Configuration

---
+++ src/Olx/Olx.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Olx.Application.Abstractions;
+using Olx.Domain.Entities;
+
+namespace Olx.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext, IAppDbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<Announcements> Announcements { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+
+    async ValueTask<int> IAppDbContext.SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+}
