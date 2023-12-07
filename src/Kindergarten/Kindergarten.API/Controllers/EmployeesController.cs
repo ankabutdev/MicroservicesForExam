43be2645406721ce21e2f8d15f81dc1166fd6918
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 17:02:14 +0500

    Added cache

--- src/Kindergarten/Kindergarten.API/Controllers/EmployeesController.cs
+++ src/Kindergarten/Kindergarten.API/Controllers/EmployeesController.cs
@@ -2,8 +2,10 @@
 using Kindergarten.Application.UseCases.EmpoloyeeCase.Commands;
 using Kindergarten.Application.UseCases.EmpoloyeeCase.Queries;
 using Kindergarten.Domain.Dtos.Employees;
+using Kindergarten.Domain.Entities.Employees;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Kindergarten.API.Controllers;
 
@@ -13,17 +15,34 @@ public class EmployeesController : ControllerBase
 {
     private readonly IMapper _mapper;
     private readonly IMediator _mediator;
+    private readonly IMemoryCache _cache;
 
-    public EmployeesController(IMapper mapper, IMediator mediator)
+    public EmployeesController(IMapper mapper, IMediator mediator, IMemoryCache cache)
     {
         _mapper = mapper;
         _mediator = mediator;
+        _cache = cache;
     }
 
     [HttpGet]
     public async Task<IActionResult> GetAllAsync()
     {
+        if (_cache.TryGetValue("AllAdmins", out var cachedData))
+        {
+            var emp = (IEnumerable<Employee>)cachedData!;
+            return Ok(emp);
+        }
+
         var result = await _mediator.Send(new GetAllEmpQuery());
+
+        var cacheEntryOptions = new MemoryCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1),
+            SlidingExpiration = TimeSpan.FromSeconds(20)
+        };
+
+        _cache.Set("AllEmployees", result, cacheEntryOptions);
+
         return Ok(result);
     }
 
