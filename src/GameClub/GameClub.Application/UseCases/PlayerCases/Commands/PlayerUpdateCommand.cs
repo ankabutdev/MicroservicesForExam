d9c1a4c328dbd56f88d815b8fe318848bb83b44f
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 12:40:01 +0500

    UpdatedDtos Improved

--- src/GameClub/GameClub.Application/UseCases/PlayerCases/Commands/PlayerUpdateCommand.cs
+++ src/GameClub/GameClub.Application/UseCases/PlayerCases/Commands/PlayerUpdateCommand.cs
@@ -1,9 +1,13 @@
 ﻿using MediatR;
+using System.Text.Json.Serialization;
 
 namespace GameClub.Application.UseCases.PlayerCases.Commands;
 
 public class PlayerUpdateCommand : IRequest<bool>
 {
+    [JsonIgnore]
+    public long Id { get; set; }
+
     public string NickName { get; set; }
 
     public long HoursCount { get; set; }
