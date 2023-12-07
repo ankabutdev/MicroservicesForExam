35e6249620e0f764169bbce97d9d113c76bb7ae5
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 18:40:01 +0500

    Added Configuration

---
+++ src/Olx/Olx.Infrastructure/Persistence/EntityTypeConfiguration/AnnouncementConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Olx.Domain.Entities;
+
+namespace Olx.Infrastructure.Persistence.EntityTypeConfiguration;
+
+public class AnnouncementConfiguration : IEntityTypeConfiguration<Announcements>
+{
+    public void Configure(EntityTypeBuilder<Announcements> builder)
+    {
+        builder.HasKey(a => a.Id);
+        builder.Property(a => a.Name).IsRequired();
+        builder.Property(a => a.Description);
+        builder.Property(a => a.UserId).IsRequired();
+        builder.Property(a => a.CategortId).IsRequired();
+        builder.Property(a => a.Title).IsRequired();
+        builder.Property(a => a.ImagePath);
+        builder.Property(a => a.CreatedAt).IsRequired();
+        builder.Property(a => a.UpdatedAt).IsRequired();
+
+        builder.HasOne(a => a.User)
+            .WithMany(u => u.Announcements)
+            .HasForeignKey(a => a.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(a => a.Category)
+            .WithMany(c => c.Announcements)
+            .HasForeignKey(a => a.CategortId)
+            .OnDelete(DeleteBehavior.SetNull);
+    }
+}
