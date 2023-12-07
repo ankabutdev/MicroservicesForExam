c0a2f640595265311de37ac09beeb225c8f5168c
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 14:11:53 +0500

    Completed Fluint Api And

---
+++ src/Kindergarten/Kindergarten.Infrastructure/Persistence/EntityTypeConfiguration/Employees/EmployeeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Kindergarten.Domain.Entities.Employees;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Kindergarten.Infrastructure.Persistence.EntityTypeConfiguration.Employees;
+
+public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+{
+    public void Configure(EntityTypeBuilder<Employee> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .UseIdentityColumn();
+
+        builder.Property(x => x.FullName)
+            .HasMaxLength(256)
+            .IsRequired();
+
+        builder.HasIndex(x => x.Email)
+            .IsUnique();
+
+        builder.Property(x => x.PhoneNumber)
+            .IsRequired();
+    }
+}
