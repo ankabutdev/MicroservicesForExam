1532b8172c36ae5aba1eb15277b6a0cf92611004
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 14:13:40 +0500

    Improve

---
+++ src/Kindergarten/Kindergarten.Infrastructure/Persistence/EntityTypeConfiguration/Teachers/TeacherConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Kindergarten.Domain.Entities.Teachers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Kindergarten.Infrastructure.Persistence.EntityTypeConfiguration.Teachers;
+
+public class TeacherConfiguration : IEntityTypeConfiguration<Teacher>
+{
+    public void Configure(EntityTypeBuilder<Teacher> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .UseIdentityColumn();
+
+        builder.Property(t => t.FullName).IsRequired().HasMaxLength(100);
+        builder.Property(t => t.Email).HasMaxLength(255);
+        builder.Property(t => t.PhoneNumber).HasMaxLength(20);
+        builder.Property(t => t.Address).HasMaxLength(255);
+
+        builder.HasMany(t => t.Groups).WithMany(g => g.Teachers);
+    }
+}
