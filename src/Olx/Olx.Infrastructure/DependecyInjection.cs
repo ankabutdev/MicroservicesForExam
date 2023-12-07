3e8a8b03562c682cc36f7174ffba69a382a8cd59
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 18:40:26 +0500

    Improve

---
+++ src/Olx/Olx.Infrastructure/DependecyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Olx.Application.Abstractions;
+using Olx.Infrastructure.Persistence;
+
+namespace Olx.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var dockerConnection = configuration.GetConnectionString("DockerConnection");
+        var defaultConnection = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<IAppDbContext, AppDbContext>(options =>
+        {
+            options.UseSqlServer(dockerConnection);
+        });
+
+        return services;
+    }
+}
+
