6e418e6d8245e1cfaa9743534e75cfddeced4499
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 11:50:28 +0500

    added cofigurations

---
+++ src/Nikoh/Nikoh.Infrastructure/Persitence/EntityTypeConfiguration/MarriageConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NIkoh.Domain.Entities.Marriages;
+
+namespace Nikoh.Infrastructure.Persitence.EntityTypeConfiguration;
+
+public class MarriageConfiguration : IEntityTypeConfiguration<Marriage>
+{
+    public void Configure(EntityTypeBuilder<Marriage> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.WomenId)
+            .IsRequired();
+
+
+    }
+}
