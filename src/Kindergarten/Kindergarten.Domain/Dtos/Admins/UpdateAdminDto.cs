b7de5abe30687261bb755297afcfe7ffcee51291
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 12:40:46 +0500

    UpdatedDtos Improved

--- src/Kindergarten/Kindergarten.Domain/Dtos/Admins/UpdateAdminDto.cs
+++ src/Kindergarten/Kindergarten.Domain/Dtos/Admins/UpdateAdminDto.cs
@@ -1,7 +1,10 @@
-﻿namespace Kindergarten.Domain.Dtos.Admins;
+﻿using System.Text.Json.Serialization;
+
+namespace Kindergarten.Domain.Dtos.Admins;
 
 public class UpdateAdminDto
 {
+    [JsonIgnore]
     public long Id { get; set; }
 
     public string Name { get; set; }
