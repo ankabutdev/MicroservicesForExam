0524a90f5d16d168301fa6a2bac851b36c340b5c
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 17:02:38 +0500

    Added cache

--- src/Nikoh/Nikoh.API/Controllers/PersonsController.cs
+++ src/Nikoh/Nikoh.API/Controllers/PersonsController.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Nikoh.Application.UseCases.PersonCases.Commands;
 using Nikoh.Application.UseCases.PersonCases.Queris;
 using NIkoh.Domain.Dtos.Persons;
+using NIkoh.Domain.Entities.Persons;
 
 namespace Nikoh.API.Controllers;
 
@@ -13,18 +15,33 @@ public class PersonsController : ControllerBase
 {
     private readonly IMapper _mapper;
     private readonly IMediator _mediator;
+    private readonly IMemoryCache _cache;
 
-    public PersonsController(IMapper mapper, IMediator mediator)
+    public PersonsController(IMapper mapper, IMediator mediator, IMemoryCache cache)
     {
         _mapper = mapper;
         _mediator = mediator;
+        _cache = cache;
     }
 
     [HttpGet]
     public async Task<IActionResult> GetAllAsync()
     {
-        var result = await _mediator
-            .Send(new GetAllPersonQuery());
+        if (_cache.TryGetValue("AllAdmins", out var cachedData))
+        {
+            var persons = (IEnumerable<Person>)cachedData!;
+            return Ok(persons);
+        }
+
+        var result = await _mediator.Send(new GetAllPersonQuery());
+
+        var cacheEntryOptions = new MemoryCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1),
+            SlidingExpiration = TimeSpan.FromSeconds(20)
+        };
+
+        _cache.Set("AllPersons", result, cacheEntryOptions);
 
         return Ok(result);
     }
