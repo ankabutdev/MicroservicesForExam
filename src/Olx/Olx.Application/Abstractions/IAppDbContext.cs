e10d7d0a927f99e80506c1381709ff989b837c56
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 18:39:03 +0500

    improve

---
+++ src/Olx/Olx.Application/Abstractions/IAppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Olx.Domain.Entities;
+
+namespace Olx.Application.Abstractions;
+
+public interface IAppDbContext
+{
+    DbSet<User> Users { get; set; }
+
+    DbSet<Announcements> Announcements { get; set; }
+
+    DbSet<Category> Categories { get; set; }
+
+    ValueTask<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
