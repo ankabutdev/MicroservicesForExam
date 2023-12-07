103b185ea5140473c422247266da44e03bf6e061
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 17:02:06 +0500

    Added cache

--- src/Kindergarten/Kindergarten.API/Controllers/AdminsController.cs
+++ src/Kindergarten/Kindergarten.API/Controllers/AdminsController.cs
@@ -2,8 +2,10 @@
 using Kindergarten.Application.UseCases.AdminCase.Queries;
 using Kindergarten.Application.UseCases.AdminCases.Commands;
 using Kindergarten.Domain.Dtos.Admins;
+using Kindergarten.Domain.Entities.Admins;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Kindergarten.API.Controllers;
 
@@ -13,17 +15,34 @@ public class AdminsController : ControllerBase
 {
     private readonly IMediator _mediator;
     private readonly IMapper _mapper;
+    private readonly IMemoryCache _cache;
 
-    public AdminsController(IMediator mediator, IMapper mapper)
+    public AdminsController(IMediator mediator, IMapper mapper, IMemoryCache cache)
     {
         _mediator = mediator;
         _mapper = mapper;
+        _cache = cache;
     }
 
     [HttpGet]
     public async Task<IActionResult> GetAllAsync()
     {
+        if (_cache.TryGetValue("AllAdmins", out var cachedData))
+        {
+            var admin = (IEnumerable<Admin>)cachedData!;
+            return Ok(admin);
+        }
+
         var result = await _mediator.Send(new GetAllAdminQuery());
+
+        var cacheEntryOptions = new MemoryCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1),
+            SlidingExpiration = TimeSpan.FromSeconds(20)
+        };
+
+        _cache.Set("AllAdmins", result, cacheEntryOptions);
+
         return Ok(result);
     }
 
