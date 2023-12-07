df2e8f5b6b5df3b59e51612888c5179046a42e49
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 12:40:06 +0500

    UpdatedDtos Improved

--- src/GameClub/GameClub.Domain/DTOs/Admins/AdminUpdateDto.cs
+++ src/GameClub/GameClub.Domain/DTOs/Admins/AdminUpdateDto.cs
@@ -1,7 +1,10 @@
-﻿namespace GameClub.Domain.DTOs.Admins;
+﻿using System.Text.Json.Serialization;
+
+namespace GameClub.Domain.DTOs.Admins;
 
 public class AdminUpdateDto
 {
+    [JsonIgnore]
     public int Id { get; set; }
 
     public string Name { get; set; }
