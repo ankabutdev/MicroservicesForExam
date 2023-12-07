e1078b287cf1e1db003bd5afec8b2fdadd1931f7
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 18:39:39 +0500

    Add entities

---
+++ src/Olx/Olx.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿namespace Olx.Domain.Entities;
+
+public class Category
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public ICollection<Announcements> Announcements { get; set; }
+}
---
+++ src/Olx/Olx.Domain/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using Olx.Domain.Enums;
+
+namespace Olx.Domain.Entities;
+
+public class User
+{
+    public long Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string UserName { get; set; }
+
+    public string Address { get; set; }
+
+    public Role Role { get; set; }
+
+    public ICollection<Announcements> Announcements { get; set; }
+}
