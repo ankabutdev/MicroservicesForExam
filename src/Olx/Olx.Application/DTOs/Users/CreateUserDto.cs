14e7c5c2fd3014892fd51b7a267543eb47e021da
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 18:39:23 +0500

    Added Dtos

---
+++ src/Olx/Olx.Application/DTOs/Users/CreateUserDto.cs
@@ -0,0 +1,21 @@
+﻿using Olx.Domain.Enums;
+
+namespace Olx.Application.DTOs.Users;
+
+public class CreateUserDto
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string UserName { get; set; }
+
+    public string Address { get; set; }
+
+    public Role Role { get; set; }
+
+}
