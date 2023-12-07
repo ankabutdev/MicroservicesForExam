8c98fc6c9f4b2097b564cc1e8d3d1a9da3376139
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 18:39:31 +0500

    improve

---
+++ src/Olx/Olx.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Olx.Application.Mappers;
+using System.Reflection;
+
+namespace Olx.Application;
+
+public static class DepandecyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddAutoMapper(typeof(MappingConfiguration));
+        return services;
+    }
+}
