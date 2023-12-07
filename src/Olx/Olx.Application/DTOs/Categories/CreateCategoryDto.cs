38a1d390c4e4e07431b3274052205b8d2ee73ec9
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-07 18:39:12 +0500

    improve

---
+++ src/Olx/Olx.Application/DTOs/Categories/CreateCategoryDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Olx.Application.DTOs.Categories;
+
+public class CreateCategoryDto
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ src/Olx/Olx.Application/DTOs/Categories/UpdateCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Olx.Application.DTOs.Categories;
+
+public class UpdateCategoryDto
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
