4d812ad43957ccdcc1ae455309330c8922c8160d
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-10 21:04:33 +0500

    add AnnouncementsControllers

---
+++ src/Olx/Olx.API/Controllers/AnnouncementsController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Olx.Application.UseCases.Announcements.Queries.GetAllAnnouncement;
+
+namespace Olx.API.Controllers;
+
+[Route("api/announcements")]
+[ApiController]
+public class AnnouncementsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+
+    public AnnouncementsController(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var result = await _mediator
+            .Send(new GetAllAnnouncementQuery());
+
+        return Ok(result);
+    }
+}
