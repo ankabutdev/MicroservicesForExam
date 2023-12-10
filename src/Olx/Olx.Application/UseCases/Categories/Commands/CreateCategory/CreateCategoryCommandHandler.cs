ab78651f92279a0aaefcc3a61591b692f54717a6
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-10 21:01:15 +0500

    new feature added in UseCases

---
+++ src/Olx/Olx.Application/UseCases/Categories/Commands/CreateCategory/CreateCategoryCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MediatR;
+using Olx.Application.Abstractions;
+
+namespace Olx.Application.UseCases.Categories.Commands.CreateCategory;
+
+public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand, bool>
+{
+    private readonly IMapper _mapper;
+    private readonly IAppDbContext _context;
+
+    public CreateCategoryCommandHandler(IMapper mapper, IAppDbContext context)
+    {
+        _mapper = mapper;
+        _context = context;
+    }
+
+    public Task<bool> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
