40862ff8c51e1fcf56d9702a110f666118a9c73d
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-10 21:00:57 +0500

    new feature added in UseCases

---
+++ src/Olx/Olx.Application/UseCases/Announcements/Queries/GetByIdAnnouncement/GetByIdAnnouncementQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Olx.Domain.Entities;
+
+namespace Olx.Application.UseCases.Announcements.Queries.GetByIdAnnouncement;
+
+public class GetByIdAnnouncementQuery : IRequest<Announcement>
+{
+    public long Id { get; set; }
+}
