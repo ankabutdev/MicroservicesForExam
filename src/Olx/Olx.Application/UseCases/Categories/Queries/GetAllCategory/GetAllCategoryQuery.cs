0897ea21fb034dcd4b577b3b50cca9d40de02265
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-10 21:01:38 +0500

    new feature added in UseCases

---
+++ src/Olx/Olx.Application/UseCases/Categories/Queries/GetAllCategory/GetAllCategoryQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Olx.Domain.Entities;
+
+namespace Olx.Application.UseCases.Categories.Queries.GetAllCategory;
+
+public class GetAllCategoryQuery : IRequest<IEnumerable<Category>>
+{
+}
