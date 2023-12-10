47b3375b2c2a94b0f369880afade15d97906fd01
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-10 21:01:45 +0500

    new feature added in UseCases

---
+++ src/Olx/Olx.Application/UseCases/Categories/Queries/GetCategoryById/GetCategoryByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Olx.Domain.Entities;
+
+namespace Olx.Application.UseCases.Categories.Queries.GetCategoryById;
+
+public class GetCategoryByIdQuery : IRequest<Category>
+{
+    public long Id { get; set; }
+}
