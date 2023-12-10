60f5e3109c8e3deced32fcec5b16834e9902302e
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-10 21:02:08 +0500

    new feature added in UseCases

---
+++ src/Olx/Olx.Application/UseCases/Users/Commands/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Olx.Domain.Enums;
+
+namespace Olx.Application.UseCases.Users.Commands.UpdateUser;
+
+public class UpdateUserCommand : IRequest<bool>
+{
+    public long Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public string UserName { get; set; }
+
+    public string Address { get; set; }
+
+    public Role Role { get; set; }
+
+}
