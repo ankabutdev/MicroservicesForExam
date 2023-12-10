f3735e53eba6244f2362071045f94405ea69caca
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-10 21:00:40 +0500

    new feature added in UseCases

---
+++ src/Olx/Olx.Application/UseCases/Announcements/Commands/UpdateAnnouncement/UpdateAnnouncementCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+
+namespace Olx.Application.UseCases.Announcements.Commands.UpdateAnnouncement;
+
+public class UpdateAnnouncementCommand : IRequest<bool>
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; }
+
+    public long UserId { get; set; }
+
+    public string Description { get; set; }
+
+    public long CategortId { get; set; }
+
+    public string Title { get; set; }
+
+    public string ImagePath { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
