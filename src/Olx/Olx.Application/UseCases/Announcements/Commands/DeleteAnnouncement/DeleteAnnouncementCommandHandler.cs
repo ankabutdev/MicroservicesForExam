bee2fdb4fe1c77c9a04f8e65b82ba53557c301d5
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-10 21:00:36 +0500

    new feature added in UseCases

---
+++ src/Olx/Olx.Application/UseCases/Announcements/Commands/DeleteAnnouncement/DeleteAnnouncementCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MediatR;
+using Olx.Application.Abstractions;
+
+namespace Olx.Application.UseCases.Announcements.Commands.DeleteAnnouncement;
+
+public class DeleteAnnouncementCommandHandler : IRequestHandler<DeleteAnnouncementCommand, bool>
+{
+    private readonly IMapper _mapper;
+    private readonly IAppDbContext _context;
+
+    public DeleteAnnouncementCommandHandler(IMapper mapper, IAppDbContext context)
+    {
+        _mapper = mapper;
+        _context = context;
+    }
+
+    public Task<bool> Handle(DeleteAnnouncementCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
