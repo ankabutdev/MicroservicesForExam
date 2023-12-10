3f083cc26ff1db16e4c65ae752a33ab031522293
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-10 21:01:29 +0500

    new feature added in UseCases

---
+++ src/Olx/Olx.Application/UseCases/Categories/Commands/UpdateCategory/UpdateCategoryCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace Olx.Application.UseCases.Categories.Commands.UpdateCategory;
+
+public class UpdateCategoryCommand : IRequest<bool>
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+}
