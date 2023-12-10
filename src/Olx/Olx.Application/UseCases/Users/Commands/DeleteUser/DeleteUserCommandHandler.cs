407629e391683033c3da8a5e87fbad300cc85f0c
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-12-10 21:02:04 +0500

    new feature added in UseCases

---
+++ src/Olx/Olx.Application/UseCases/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MediatR;
+using Olx.Application.Abstractions;
+
+namespace Olx.Application.UseCases.Users.Commands.DeleteUser;
+
+public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, bool>
+{
+    private readonly IMapper _mapper;
+    private readonly IAppDbContext _context;
+
+    public DeleteUserCommandHandler(IMapper mapper, IAppDbContext context)
+    {
+        _mapper = mapper;
+        _context = context;
+    }
+
+    public Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
