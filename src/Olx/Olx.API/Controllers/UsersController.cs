d9d93ff249dab13523505f373314a83dca8b14dc
ankabutdev <Ankabut571@gmail.com>
2023-12-13 23:06:03 +0500

    improve

--- src/Olx/Olx.API/Controllers/UsersController.cs
+++ src/Olx/Olx.API/Controllers/UsersController.cs
@@ -1,7 +1,12 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Olx.Application.DTOs.Users;
+using Olx.Application.UseCases.Users.Commands.CreateUser;
+using Olx.Application.UseCases.Users.Commands.DeleteUser;
+using Olx.Application.UseCases.Users.Commands.UpdateUser;
 using Olx.Application.UseCases.Users.Queries.GetAllUsers;
+using Olx.Application.UseCases.Users.Queries.GetUserById;
 
 namespace Olx.API.Controllers;
 
@@ -23,6 +28,41 @@ public class UsersController : ControllerBase
     {
         var result = await _mediator
             .Send(new GetAllUsersQuery());
+
+        return Ok(result);
+    }
+
+    [HttpGet("{Id}")]
+    public async Task<IActionResult> GetByIdAsync(long Id)
+    {
+        var result = await _mediator
+            .Send(new GetUserByIdQuery() { Id = Id });
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(CreateUserDto dto)
+    {
+        var user = _mapper.Map<CreateUserCommand>(dto);
+        var result = await _mediator.Send(user);
+        return Ok(result);
+    }
+
+    [HttpPut("{Id}")]
+    public async Task<IActionResult> UpdateAsync(long Id, UpdateUserDto dto)
+    {
+        var user = _mapper.Map<UpdateUserCommand>(dto);
+        user.Id = Id;
+        var result = await _mediator.Send(user);
+        return Ok(result);
+    }
+
+    [HttpDelete("{Id}")]
+    public async Task<IActionResult> DeleteAsync(long Id)
+    {
+        var result = await _mediator
+            .Send(new DeleteUserCommand() { Id = Id });
 
         return Ok(result);
     }
