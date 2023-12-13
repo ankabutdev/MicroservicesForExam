4a10e40c548bab411d1d7477910715db1ee05c56
ankabutdev <Ankabut571@gmail.com>
2023-12-13 23:07:30 +0500

    improve

---
+++ src/Olx/Olx.Application/Services/Files/FileService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Olx.Application.Common.Helpers;
+using Olx.Application.Interfaces.Files;
+
+namespace Olx.Application.Services.Files;
+
+public class FileService : IFileService
+{
+    private readonly string MEDIA = "media";
+    private readonly string IMAGES = "images";
+    private readonly string ROOTPATH;
+
+    public FileService(IWebHostEnvironment env)
+    {
+        ROOTPATH = env.WebRootPath;
+    }
+
+    public async Task<bool> DeleteImageAsync(string subpath)
+    {
+        string path = Path.Combine(ROOTPATH, subpath);
+        if (File.Exists(path))
+        {
+            await Task.Run(() =>
+            {
+                File.Delete(path);
+            });
+            return true;
+        }
+        else return false;
+    }
+
+    public async Task<string> UploadImageAsync(IFormFile image)
+    {
+        string newImageName = MediaHelper.MakeImageName(image.FileName);
+        string subpath = Path.Combine(MEDIA, IMAGES, newImageName);
+        string path = Path.Combine(ROOTPATH, subpath);
+
+        var stream = new FileStream(path, FileMode.Create);
+        await image.CopyToAsync(stream);
+        stream.Close();
+
+        return subpath;
+    }
+}
