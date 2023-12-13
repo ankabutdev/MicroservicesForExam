81b0896fabe4b7003569c0ae828da87e0a562337
ankabutdev <Ankabut571@gmail.com>
2023-12-13 23:07:24 +0500

    improve

--- src/Olx/Olx.Application/Mappers/MappingConfiguration.cs
+++ src/Olx/Olx.Application/Mappers/MappingConfiguration.cs
@@ -1,4 +1,14 @@
 ﻿using AutoMapper;
+using Olx.Application.DTOs.Announcements;
+using Olx.Application.DTOs.Categories;
+using Olx.Application.DTOs.Users;
+using Olx.Application.UseCases.Announcements.Commands.CreateAnnouncement;
+using Olx.Application.UseCases.Announcements.Commands.UpdateAnnouncement;
+using Olx.Application.UseCases.Categories.Commands.CreateCategory;
+using Olx.Application.UseCases.Categories.Commands.UpdateCategory;
+using Olx.Application.UseCases.Users.Commands.CreateUser;
+using Olx.Application.UseCases.Users.Commands.UpdateUser;
+using Olx.Domain.Entities;
 
 namespace Olx.Application.Mappers;
 
@@ -6,6 +16,25 @@ public class MappingConfiguration : Profile
 {
     public MappingConfiguration()
     {
-        
+        // Announcements
+        CreateMap<Announcement, CreateAnnouncementCommand>().ReverseMap();
+        CreateMap<Announcement, UpdateAnnouncementCommand>().ReverseMap();
+
+        CreateMap<CreateAnnouncementCommand, CreateAnnouncementDto>().ReverseMap();
+        CreateMap<UpdateAnnouncementCommand, UpdateAnnouncementDto>().ReverseMap();
+
+        // Users
+        CreateMap<User, CreateUserCommand>().ReverseMap();
+        CreateMap<User, UpdateUserCommand>().ReverseMap();
+
+        CreateMap<CreateUserCommand, CreateUserDto>().ReverseMap();
+        CreateMap<UpdateUserCommand, UpdateUserDto>().ReverseMap();
+
+        // Categories
+        CreateMap<Category, CreateCategoryCommand>().ReverseMap();
+        CreateMap<Category, UpdateCategoryCommand>().ReverseMap();
+
+        CreateMap<CreateCategoryCommand, CreateCategoryDto>().ReverseMap();
+        CreateMap<UpdateCategoryCommand, UpdateCategoryDto>().ReverseMap();
     }
 }
