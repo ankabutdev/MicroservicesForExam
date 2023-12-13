f47abeef8f394f0b3a33c12d3ecee09266e8c58b
ankabutdev <Ankabut571@gmail.com>
2023-12-13 23:05:56 +0500

    improve

--- src/Olx/Olx.API/Controllers/CategoriesController.cs
+++ src/Olx/Olx.API/Controllers/CategoriesController.cs
@@ -1,7 +1,12 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Olx.Application.DTOs.Categories;
+using Olx.Application.UseCases.Categories.Commands.CreateCategory;
+using Olx.Application.UseCases.Categories.Commands.DeleteCategory;
+using Olx.Application.UseCases.Categories.Commands.UpdateCategory;
 using Olx.Application.UseCases.Categories.Queries.GetAllCategory;
+using Olx.Application.UseCases.Categories.Queries.GetCategoryById;
 
 namespace Olx.API.Controllers;
 
@@ -23,6 +28,41 @@ public class CategoriesController : ControllerBase
     {
         var result = await _mediator
             .Send(new GetAllCategoryQuery());
+
+        return Ok(result);
+    }
+
+    [HttpGet("{Id}")]
+    public async Task<IActionResult> GetByIdAsync(long Id)
+    {
+        var result = await _mediator
+            .Send(new GetCategoryByIdQuery() { Id = Id });
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(CreateCategoryDto dto)
+    {
+        var category = _mapper.Map<CreateCategoryCommand>(dto);
+        var result = await _mediator.Send(category);
+        return Ok(result);
+    }
+
+    [HttpPut("{Id}")]
+    public async Task<IActionResult> UpdateAsync(long Id, UpdateCategoryDto dto)
+    {
+        var category = _mapper.Map<UpdateCategoryCommand>(dto);
+        category.Id = Id;
+        var result = await _mediator.Send(category);
+        return Ok(result);
+    }
+
+    [HttpDelete("{Id}")]
+    public async Task<IActionResult> DeleteAsync(long Id)
+    {
+        var result = await _mediator
+            .Send(new DeleteCategoryCommand() { Id = Id });
 
         return Ok(result);
     }
