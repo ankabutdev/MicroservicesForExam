b6a48aa4226faf1c7db2b6c976bc08373c29367d
ankabutdev <Ankabut571@gmail.com>
2023-12-13 23:08:56 +0500

    improve

--- src/Olx/Olx.Application/UseCases/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
+++ src/Olx/Olx.Application/UseCases/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Olx.Application.Abstractions;
 
 namespace Olx.Application.UseCases.Users.Commands.DeleteUser;
@@ -15,8 +16,22 @@ public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, bool>
         _context = context;
     }
 
-    public Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+    public async Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        if (request.Id <= 0)
+            return false;
+
+        var user = await _context.Users
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+        if (user == null)
+            return false;
+
+        _context.Users.Remove(user);
+
+        var result = await _context
+            .SaveChangesAsync(cancellationToken);
+
+        return result > 0;
     }
 }
